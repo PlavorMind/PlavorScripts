53e2a0a053330de3e448cd69abaccfd40151381c
PlavorSeol <pseol2190@outlook.com>
2019-02-22 18:52:30 +0900

    Update install.ps1
    Incompleted

--- Windows/nginx/install.ps1
+++ Windows/nginx/install.ps1
@@ -10,46 +10,28 @@ if (!($isWindows))
 {"Your operating system is not supported."
 exit}
 
+"Downloading Configurations repository archive"
+Invoke-WebRequest "https://github.com/PlavorMind/Configurations/archive/Main.zip" -OutFile "${tempdir}/Configurations.zip"
+if (Test-Path "${tempdir}/Configurations.zip")
+{"Extracting"
+Expand-Archive "${tempdir}/Configurations.zip" $env:temp -Force
+"Deleting a temporary file"
+Remove-Item "${tempdir}/Configurations.zip" -Force}
+else
+{"Configurations repository archive download is failed!"
+exit}
+
 "Downloading nginx archive"
 Invoke-WebRequest "http://nginx.org/download/nginx-${version}.zip" -OutFile "${tempdir}/nginx.zip"
 if (Test-Path "${tempdir}/nginx.zip")
 {"Extracting"
 Expand-Archive "${tempdir}/nginx.zip" $tempdir -Force
 "Deleting a temporary file"
-Remove-Item "${env:temp}\nginx.zip" -Force
+Remove-Item "${tempdir}/nginx.zip" -Force
 "Renaming nginx directory"
-Move-Item "${env:temp}\nginx-*" "${env:temp}\nginx" -Force}
+Move-Item "${tempdir}/nginx-*" "${tempdir}/nginx" -Force}
 else
 {"nginx archive download is failed!"
-exit}
-
-"Downloading Configurations repository archive"
-Invoke-WebRequest "https://github.com/PlavorMind/Configurations/archive/Main.zip" -OutFile "${env:temp}\Configurations.zip"
-if (Test-Path "${env:temp}\Configurations.zip")
-{"Extracting"
-Expand-Archive "${env:temp}\Configurations.zip" $env:temp -Force
-"Deleting a temporary file"
-Remove-Item "${env:temp}\Configurations.zip" -Force}
-else
-{"Configurations repository archive download is failed!"
-exit}
-
-"Downloading create_service.ps1 file"
-Invoke-WebRequest "https://raw.githubusercontent.com/PlavorSeol/Scripts/Main/Windows/nginx/install_data/create_service.ps1" -OutFile "${env:temp}\nginx\create_service.ps1"
-if (!(Test-Path "${env:temp}\nginx\create_service.ps1"))
-{"create_service.ps1 file download is failed!"
-exit}
-
-"Downloading fetch_nginx_conf.ps1 file"
-Invoke-WebRequest "https://raw.githubusercontent.com/PlavorSeol/Scripts/Main/Windows/nginx/install_data/fetch_nginx_conf.ps1" -OutFile "${env:temp}\nginx\fetch_nginx_conf.ps1"
-if (!(Test-Path "${env:temp}\nginx\fetch_nginx_conf.ps1"))
-{"fetch_nginx_conf.ps1 file download is failed!"
-exit}
-
-"Downloading remove_service.ps1 file"
-Invoke-WebRequest "https://raw.githubusercontent.com/PlavorSeol/Scripts/Main/Windows/nginx/install_data/remove_service.ps1" -OutFile "${env:temp}\nginx\remove_service.ps1"
-if (!(Test-Path "${env:temp}\nginx\remove_service.ps1"))
-{"remove_service.ps1 file download is failed!"
 exit}
 
 ."${env:temp}\nginx\fetch_nginx_conf.ps1"
