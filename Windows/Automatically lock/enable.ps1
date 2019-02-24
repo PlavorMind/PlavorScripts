bd28771063514f41ce436906ecd2a19e0f9fd404
PlavorSeol <pseol2190@outlook.com>
2019-02-24 21:34:43 +0900

    Remove string output

--- Windows/Automatically lock/enable.ps1
+++ Windows/Automatically lock/enable.ps1
@@ -15,5 +15,4 @@ if ($allusers)
 else
 {$path="${env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk"}
 
-"Enabling automatically lock"
 ."${PSScriptRoot}/../../modules/CreateShortcut.ps1" -arguments "user32.dll,LockWorkStation" -path $path -target "C:/Windows/System32/rundll32.exe"
