e6c17ca2db846ae98eb896277356ea47c54997d9
PlavorSeol <pseol2190@outlook.com>
2019-02-24 21:40:21 +0900

    Remove string output

--- Windows/Dolphin/install.ps1
+++ Windows/Dolphin/install.ps1
@@ -4,7 +4,7 @@
 param
 ([string]$dir="C:/Program Files/Dolphin", #Directory to install
 [switch]$portable, #Installs as portable mode if this is set
-[string]$version="5.0-9616") #Version to install
+[string]$version="5.0-9620") #Version to install
 
 ."${PSScriptRoot}/../../modules/OSDetectorDebug.ps1"
 ."${PSScriptRoot}/../../modules/SetTempDir.ps1"
@@ -14,14 +14,10 @@ if (!($isWindows))
 exit}
 
 if (Test-Path "C:/Program Files/7-Zip/7z.exe")
-{"Downloading Dolphin archive"
-Invoke-WebRequest "https://dl.dolphin-emu.org/builds/dolphin-master-${version}-x64.7z" -OutFile "${tempdir}/Dolphin.7z"
+{Invoke-WebRequest "https://dl.dolphin-emu.org/builds/dolphin-master-${version}-x64.7z" -OutFile "${tempdir}/Dolphin.7z"
 if (Test-Path "${tempdir}/Dolphin.7z")
-  {"Extracting with 7-Zip"
-  ."C:/Program Files/7-Zip/7z.exe" x "${tempdir}/Dolphin.7z" -aoa -bt -o"${tempdir}" -spe -y
-  "Deleting a temporary file"
+  {."C:/Program Files/7-Zip/7z.exe" x "${tempdir}/Dolphin.7z" -aoa -bt -o"${tempdir}" -spe -y
   Remove-Item "${tempdir}/Dolphin.7z" -Force
-  "Moving Dolphin directory"
   Move-Item "${tempdir}/Dolphin-x64" $dir -Force}
 else
   {"Cannot download Dolphin archive."
