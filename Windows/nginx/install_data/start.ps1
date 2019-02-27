721dd01f05f1eea5df2206e9156e79f82452af65
PlavorSeol <pseol2190@outlook.com>
2019-02-28 05:59:39 +0900

    Revert "Call stop.ps1"

--- Windows/nginx/install_data/start.ps1
+++ Windows/nginx/install_data/start.ps1
@@ -1,6 +1,9 @@
 #Start nginx
 #Starts nginx. Restarts it if it is already running.
 
-."${PSScriptRoot}/stop.ps1"
+if (Get-Process "nginx" -ErrorAction Ignore)
+{"Stopping nginx"
+Stop-Process -Force -Name "nginx"}
+
 "Starting nginx"
 Start-Process "${PSScriptRoot}/nginx.exe" -WorkingDirectory $PSScriptRoot
