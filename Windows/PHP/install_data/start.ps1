82996a37e44c2756fbbd366a4ffac9ea1d4643f0
PlavorSeol <pseol2190@outlook.com>
2019-03-01 02:57:04 +0900

    Add files via upload

---
+++ Windows/PHP/install_data/start.ps1
@@ -0,0 +1,12 @@
+#Start CGI/FastCGI
+#Starts PHP CGI/FastCGI.
+
+param([string]$bind="0.0.0.0:90") #Bind path for CGI/FastCGI
+
+if (Get-Process "php-cgi" -ErrorAction Ignore)
+{"PHP CGI/FastCGI is already running."}
+else
+{while (!(Test-Path "${PSScriptRoot}/stop_php_cgi"))
+  {"Starting PHP CGI/FastCGI"
+  ."${PSScriptRoot}/php-cgi.exe" -b $bind}
+Remove-Item "${PSScriptRoot}/stop_php_cgi" -Force}
---
+++ Windows/PHP/install_data/stop.ps1
@@ -0,0 +1,9 @@
+#Stop CGI/FastCGI
+#Stops PHP CGI/FastCGI.
+
+if (Get-Process "php-cgi" -ErrorAction Ignore)
+{"Stopping PHP CGI/FastCGI"
+"">"${PSScriptRoot}/stop_php_cgi"
+Stop-Process -Force -Name "php-cgi"}
+else
+{"PHP CGI/FastCGI is not running."}
