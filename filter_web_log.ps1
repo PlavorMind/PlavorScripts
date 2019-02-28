7c4c545d91addfc2816cffe279a22fbc1bff22c8
PlavorSeol <pseol2190@outlook.com>
2019-02-28 11:50:34 +0900

    Update filter_web_log.ps1

--- filter_web_log.ps1
+++ filter_web_log.ps1
@@ -2,14 +2,24 @@
 #Filter web server log.
 
 param
-([switch]$blank_source,
+([switch]$blank_source, #Blanks the source file after filtering if this is set
 [string]$path="/etc/nginx/logs/Main/access_log.txt", #File path to filter
 [string]$savepath="filtered_log.txt") #Path to save filtered web server log file
 
+."${PSScriptRoot}/modules/OSDetectorDebug.ps1"
+."${PSScriptRoot}/modules/SetTempDir.ps1"
+
+$filters=@(".*127\.0\.0\.[0-9]+.*",".*192\.168\.[0-9]+\.[0-9]+.*")
+
 if (Test-Path $path)
-{Copy-Item $path $savepath -Force
-Select-String ".*127\.0\.0\.[0-9]+.*" $savepath -Encoding utf8 -NotMatch|ForEach-Object {$_.Line}>$savepath
-Select-String ".*192\.168\.[0-9]+\.[0-9]+.*" $savepath -Encoding utf8 -NotMatch|ForEach-Object {$_.Line}>$savepath
+{$num=0
+Copy-Item $path "${tempdir}/filtered_log_${num}" -Force
+foreach ($filter in $filters)
+  {$nextnum=$num+1
+  Select-String $filter "${tempdir}/filtered_log_${num}" -Encoding utf8 -NotMatch|ForEach-Object {$_.Line}>"${tempdir}/filtered_log_${nextnum}"
+  Remove-Item "${tempdir}/filtered_log_${num}" -Force
+  $num++}
+Move-Item "${tempdir}/filtered_log_${num}" $savepath -Force
 if ($blank_source)
   {"">$path}
 }
