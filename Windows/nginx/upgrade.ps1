11ba12cc49f391fe76f91e234758651d94ff1b89
PlavorSeol <pseol2190@outlook.com>
2019-03-02 10:11:04 +0900

    Create upgrade.ps1

---
+++ Windows/nginx/upgrade.ps1
@@ -0,0 +1,39 @@
+#nginx upgrader
+#Upgrades nginx.
+
+param
+([string]$dir="C:/nginx", #Directory that nginx is installed
+[string]$version="1.15.9") #Version to upgrade
+
+."${PSScriptRoot}/../../modules/OSDetectorDebug.ps1"
+."${PSScriptRoot}/../../modules/SetTempDir.ps1"
+
+if (!($isWindows))
+{"Your operating system is not supported."
+exit}
+
+"Downloading nginx archive"
+Invoke-WebRequest "http://nginx.org/download/nginx-${version}.zip" -OutFile "${tempdir}/nginx.zip"
+if (Test-Path "${tempdir}/nginx.zip")
+{"Extracting"
+Expand-Archive "${tempdir}/nginx.zip" $tempdir -Force
+"Deleting a temporary file"
+Remove-Item "${tempdir}/nginx.zip" -Force
+"Renaming nginx directory"
+Move-Item "${tempdir}/nginx-*" "${tempdir}/nginx" -Force}
+else
+{"Cannot download nginx archive."
+exit}
+
+if (Get-Process "nginx" -ErrorAction Ignore)
+{"Stopping nginx"
+Stop-Process -Force -Name "nginx"}
+
+"Moving nginx.exe file"
+Move-Item "${tempdir}/nginx/nginx.exe" $dir -Force
+
+"Deleting a temporary directory"
+Remove-Item "${tempdir}/nginx" -Force -Recurse
+
+"Starting nginx"
+Start-Process "${dir}/nginx.exe" -WorkingDirectory $dir
