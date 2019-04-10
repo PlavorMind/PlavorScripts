3ba31c01838ac2d343fb568fc78a3c5b7162a38e
PlavorSeol <pseol2190@outlook.com>
2019-04-10 20:28:45 +0900

    Fix spaces for >

--- filter_web_log.ps1
+++ filter_web_log.ps1
@@ -16,11 +16,11 @@ if (Test-Path $path)
 Copy-Item $path "${tempdir}/filtered_log_${num}" -Force
 foreach ($filter in $filters)
   {$nextnum=$num+1
-  Select-String $filter "${tempdir}/filtered_log_${num}" -Encoding utf8 -NotMatch|ForEach-Object {$_.Line}>"${tempdir}/filtered_log_${nextnum}"
+  Select-String $filter "${tempdir}/filtered_log_${num}" -Encoding utf8 -NotMatch|ForEach-Object {$_.Line} > "${tempdir}/filtered_log_${nextnum}"
   Remove-Item "${tempdir}/filtered_log_${num}" -Force
   $num++}
 Move-Item "${tempdir}/filtered_log_${num}" $savepath -Force
 if ($blank_source)
-  {#"">$path should not be used because of a bug on Windows.
+  {#"" > $path should not be used because of a bug on Windows.
   Set-Content $path "" -Encoding UTF8 -Force}
 }
