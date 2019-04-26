ef06e0498660895e7e6aa21a97a629d3e83b2995
PlavorSeol <pseol2190@outlook.com>
2019-04-26 17:16:07 +0900

    Update CreateShortcut.ps1

--- modules/CreateShortcut.ps1
+++ modules/CreateShortcut.ps1
@@ -6,11 +6,7 @@ param
 [string]$path, #Path of a shortcut
 [string]$target) #Target of a shortcut
 
-if (Test-Path "${PSScriptRoot}/OSVariables.ps1")
-{."${PSScriptRoot}/OSVariables.ps1"}
-else
-{"Cannot find OSVariables module."
-exit}
+."${PSScriptRoot}/OSVariables.ps1"
 
 if (!$isWindows)
 {"Your operating system is not supported."
