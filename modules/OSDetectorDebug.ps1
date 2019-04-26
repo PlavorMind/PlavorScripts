95dd707ccef157b5bbde7251958c0c1e32231a74
PlavorSeol <pseol2190@outlook.com>
2019-04-26 16:59:32 +0900

    Delete OSDetectorDebug.ps1

--- modules/OSDetectorDebug.ps1
+++
@@ -1,7 +0,0 @@
-#OSDetectorDebug
-#Sets variables used to detecting operating system for PowerShell 5.
-
-if (!$isLinux -and !$isMacOS -and !$isWindows)
-{$isLinux=$false
-$isMacOS=$false
-$isWindows=$true}
