ed6d70e1a723ccb8c0650a5f5acbfaa0d45dc3a7
PlavorSeol <pseol2190@outlook.com>
2019-04-26 16:51:19 +0900

    OSDetectorDebug: Detect variables and enable automatically

--- modules/OSDetectorDebug.ps1
+++ modules/OSDetectorDebug.ps1
@@ -1,9 +1,7 @@
 #OSDetectorDebug
 #Sets variables used to detecting operating system for PowerShell 5.
-#Never enable this if you are using PowerShell 6 or later.
 
-exit #Add "#" symbol to in front of this line to enable.
-
-$isLinux=$false
+if (!$isLinux -and !$isMacOS -and !$isWindows)
+{$isLinux=$false
 $isMacOS=$false
-$isWindows=$true
+$isWindows=$true}
