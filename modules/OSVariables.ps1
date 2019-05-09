2d0a8d487741c8ca7b4c335d322839b87a843ecf
PlavorSeol <pseol2190@outlook.com>
2019-05-09 16:53:14 +0900

    Merge SetTempDir to OSVariables

--- modules/OSVariables.ps1
+++ modules/OSVariables.ps1
@@ -1,8 +1,16 @@
 #OSVariables
-#Sets variables used to detecting operating system if they aren't set.
+#Sets variables used to detecting operating system and temporary directory if they aren't set.
 #This module is for backward compatiblity.
 
 if (!$isLinux -and !$isMacOS -and !$isWindows)
 {$isLinux=$false
 $isMacOS=$false
 $isWindows=$true}
+
+if ($isLinux)
+{$tempdir="/tmp"}
+elseif ($isWindows)
+{$tempdir=$Env:TEMP}
+else
+{New-Item "${PSScriptRoot}/../temp" -Force -ItemType Directory
+$tempdir="${PSScriptRoot}/../temp"}
