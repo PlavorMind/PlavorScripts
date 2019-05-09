2a9d16b0ba0127b2d432cc561260f70bc6bf3dc4
PlavorSeol <pseol2190@outlook.com>
2019-05-09 16:55:31 +0900

    Update SetTempDir.ps1

--- modules/SetTempDir.ps1
+++ modules/SetTempDir.ps1
@@ -1,12 +1,5 @@
 #SetTempDir
-#Sets $tempdir variable to temporary directory.
+#Run OSVariables module for compatibility with previous name.
 
-."${PSScriptRoot}/OSVariables.ps1"
-
-if ($isLinux)
-{$tempdir="/tmp"}
-elseif ($isWindows)
-{$tempdir=$Env:TEMP}
-else
-{New-Item "${PSScriptRoot}/../temp" -Force -ItemType Directory
-$tempdir="${PSScriptRoot}/../temp"}
+if (Test-Path "${PSScriptRoot}/OSVariables.ps1")
+{."${PSScriptRoot}/OSVariables.ps1"}
