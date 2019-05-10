93e4b23e88f980bf4eb746679b8ca8d7fea29bd9
PlavorSeol <pseol2190@outlook.com>
2019-05-10 20:35:26 +0900

    Migrate to init_script.ps1

--- Windows/set_windows_features.ps1
+++ Windows/set_windows_features.ps1
@@ -1,7 +1,7 @@
 #Set Windows features
 #Enables or disables some Windows features.
 
-."${PSScriptRoot}/../modules/OSVariables.ps1"
+."${PSScriptRoot}/../init_script.ps1"
 
 if (!$isWindows)
 {"Your operating system is not supported."
