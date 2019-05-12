e6f318bcfb17a2199e56f754874d98476f697a31
PlavorSeol <pseol2190@outlook.com>
2019-05-12 23:21:02 +0900

    Update filter_php_ini.ps1

--- filter_php_ini.ps1
+++ filter_php_ini.ps1
@@ -1,22 +1,34 @@
 #Filter php.ini
-#Filter php.ini file.
+#Filters php.ini file based on operating system.
 
 param
 ([string]$path="https://raw.githubusercontent.com/PlavorMind/Configurations/Main/php.ini", #File path or URL to filter
-[string]$savepath="/etc/php/7.2/fpm/php.ini") #Path to save filtered php.ini file
+[string]$savepath="__DEFAULT__") #Path to save filtered php.ini file
 
-$fpi_success=$false
+."${PSScriptRoot}/init_script.ps1"
 
-."${PSScriptRoot}/modules/FileURLDetector.ps1" -path $path
-if ($fud_output)
+if ($savepath -eq "__DEFAULT__")
 {if ($isLinux)
-  {Select-String ".*;windows_only.*" $fud_output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath
-  $fpi_success=$true}
+  {$savepath="/etc/php/7.2/fpm/php.ini"}
 elseif ($isWindows)
-  {Select-String ".*;linux_only.*" $fud_output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath
-  $fpi_success=$true}
+  {$savepath="C:/PHP/php.ini"}
 else
-  {"Your operating system is not supported."}
-if ($fud_web)
-  {Remove-Item $fud_output -Force}
+  {"Cannot detect default path to save."
+  exit}
 }
+
+$output=FileURLDetector $path
+if ($output)
+{"Filtering php.ini file"
+if ($isLinux)
+  {Select-String ".*;(macos|windows)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath}
+elseif ($isMacOS)
+  {Select-String ".*;(linux|windows)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath}
+elseif ($isWindows)
+  {Select-String ".*;(linux|macos)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath}
+if ($output -like "${tempdir}*")
+  {"Deleting a temporary file"
+  Remove-Item $output -Force}
+}
+else
+{"Cannot download or find php.ini file."}
