abe1dd4508e7b7cd7c9edae8c994b54d7669cd62
PlavorSeol <pseol2190@outlook.com>
2019-05-12 14:58:06 +0900

    Update filter_nginx_conf.ps1

--- filter_nginx_conf.ps1
+++ filter_nginx_conf.ps1
@@ -1,22 +1,34 @@
-#Filter nginx.conf
-#Filter nginx.conf file.
+#Filter nginx configuration
+#Filters a nginx configuration based on operating system.
 
 param
-([string]$path="https://raw.githubusercontent.com/PlavorMind/Configurations/Main/nginx.conf", #File path or URL to filter
-[string]$savepath="/etc/nginx/nginx.conf") #Path to save filtered nginx.conf file
+([string]$path="https://raw.githubusercontent.com/PlavorMind/Configurations/Main/nginx/nginx.conf", #File path or URL to filter
+[string]$savepath="__DEFAULT__") #Path to save filtered nginx configuration
 
-$fnc_success=$false
+."${PSScriptRoot}/init_script.ps1"
 
-."${PSScriptRoot}/modules/FileURLDetector.ps1" -path $path
-if ($fud_output)
+if ($savepath -eq "__DEFAULT__")
 {if ($isLinux)
-  {Select-String ".*#windows_only.*" $fud_output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath
-  $fnc_success=$true}
+  {$savepath="/etc/nginx/nginx.conf"}
 elseif ($isWindows)
-  {Select-String ".*#linux_only.*" $fud_output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath
-  $fnc_success=$true}
+  {$savepath="C:/nginx/conf/nginx.conf"}
 else
-  {"Your operating system is not supported."}
-if ($fud_web)
-  {Remove-Item $fud_output -Force}
+  {"Cannot detect default path to save."
+  exit}
 }
+
+$output=FileURLDetector $path
+if ($output)
+{"Filtering a nginx configuration"
+if ($isLinux)
+  {Select-String ".*#(macos|windows)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath}
+elseif ($IsMacOS)
+  {Select-String ".*#(linux|windows)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath}
+elseif ($isWindows)
+  {Select-String ".*#(linux|macos)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath}
+if ($output -like "${tempdir}*")
+  {"Deleting a temporary file"
+  Remove-Item $output -Force}
+}
+else
+{"Cannot download or find nginx configuration."}
