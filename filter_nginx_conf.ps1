f3b7e6eb1d5eafb50995d35a8d070e41d2d2a6ee
PlavorSeol <pseol2190@outlook.com>
2019-05-14 00:09:59 +0900

    More explicit

--- filter_nginx_conf.ps1
+++ filter_nginx_conf.ps1
@@ -13,7 +13,7 @@ if (!$destpath)
 elseif ($isWindows)
   {$destpath="C:/nginx/conf/nginx.conf"}
 else
-  {"Cannot detect default path to save."
+  {"Cannot detect default destination path."
   exit}
 }
 
