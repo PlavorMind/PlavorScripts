bafe76ffd9df0098fff57085d2efc3c4a9384784
PlavorSeol <pseol2190@outlook.com>
2019-05-14 00:03:23 +0900

    Change savepath to destpath

--- filter_php_ini.ps1
+++ filter_php_ini.ps1
@@ -2,16 +2,16 @@
 #Filters php.ini file based on operating system.
 
 param
-([string]$path="https://raw.githubusercontent.com/PlavorMind/Configurations/Main/php.ini", #File path or URL to filter
-[string]$savepath="__DEFAULT__") #Path to save filtered php.ini file
+([string]$destpath, #Destination path to save filtered php.ini file
+[string]$path="https://raw.githubusercontent.com/PlavorMind/Configurations/Main/php.ini") #File path or URL to filter
 
 ."${PSScriptRoot}/init_script.ps1"
 
-if ($savepath -eq "__DEFAULT__")
+if (!$destpath)
 {if ($isLinux)
-  {$savepath="/etc/php/7.2/fpm/php.ini"}
+  {$destpath="/etc/php/7.2/fpm/php.ini"}
 elseif ($isWindows)
-  {$savepath="C:/PHP/php.ini"}
+  {$destpath="C:/PHP/php.ini"}
 else
   {"Cannot detect default path to save."
   exit}
@@ -21,11 +21,11 @@ $output=FileURLDetector $path
 if ($output)
 {"Filtering php.ini file"
 if ($isLinux)
-  {Select-String ".*;(macos|windows)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath}
+  {Select-String ".*;(macos|windows)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $destpath}
 elseif ($isMacOS)
-  {Select-String ".*;(linux|windows)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath}
+  {Select-String ".*;(linux|windows)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $destpath}
 elseif ($isWindows)
-  {Select-String ".*;(linux|macos)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $savepath}
+  {Select-String ".*;(linux|macos)_only.*" $output -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > $destpath}
 if ($output -like "${tempdir}*")
   {"Deleting a temporary file"
   Remove-Item $output -Force}
