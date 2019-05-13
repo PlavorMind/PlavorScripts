9c65ad661327f2be6dbdd8875116dbe852cc8cb1
PlavorSeol <pseol2190@outlook.com>
2019-05-14 00:08:44 +0900

    Some migration (NOT DONE!)

--- filter_web_log.ps1
+++ filter_web_log.ps1
@@ -1,13 +1,22 @@
 #Filter web log
-#Filter web server log.
+#Filters web server log.
 
 param
 ([switch]$blank_source, #Blanks the source file after filtering if this is set
-[string]$path="/etc/nginx/logs/Main/access_log.txt", #File path to filter
-[string]$savepath="filtered.log") #Path to save filtered web server log file
+[string]$destpath="filtered.log", #Destination path to save filtered web server log file
+[string]$path) #File path to filter
 
-."${PSScriptRoot}/modules/OSDetectorDebug.ps1"
-."${PSScriptRoot}/modules/SetTempDir.ps1"
+."${PSScriptRoot}/init_script.ps1"
+
+if (!$path)
+{if ($isLinux)
+  {$destpath="/etc/nginx/logs/main/access.log"}
+elseif ($isWindows)
+  {$destpath="C:/nginx/logs/main/access.log"}
+else
+  {"Cannot detect default path."
+  exit}
+}
 
 $filters=@(".*127\.0\.0\.[0-9]+.*",".*192\.168\.[0-9]+\.[0-9]+.*")
 
@@ -19,7 +28,7 @@ foreach ($filter in $filters)
   Select-String $filter "${tempdir}/filtered_log_${num}" -Encoding utf8 -NotMatch | ForEach-Object {$_.Line} > "${tempdir}/filtered_log_${nextnum}"
   Remove-Item "${tempdir}/filtered_log_${num}" -Force
   $num++}
-Move-Item "${tempdir}/filtered_log_${num}" $savepath -Force
+Move-Item "${tempdir}/filtered_log_${num}" $destpath -Force
 if ($blank_source)
   {#"" > $path should not be used because of a bug on Windows.
   Set-Content $path "" -Encoding UTF8 -Force}
