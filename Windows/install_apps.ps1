dcaa5888c76ed0a9b8a4303efc03b835269e93b7
PlavorSeol <pseol2190@outlook.com>
2019-06-09 03:44:27 +0900

    Add qView and fix bugs for arguments for Inno installer

--- Windows/install_apps.ps1
+++ Windows/install_apps.ps1
@@ -7,6 +7,7 @@ param
 [string]$mpchc_version="1.7.13.112", #MPC-HC nightly build version to install
 [string]$obs_installer="https://github.com/obsproject/obs-studio/releases/download/23.2.0-rc1/OBS-Studio-23.2-rc1-Full-Installer-x64.exe", #URL or file path to OBS Studio installer
 [string]$python_installer="https://www.python.org/ftp/python/3.8.0/python-3.8.0a3-amd64.exe", #URL or file path to Python installer
+[string]$qview_version="2.0", #qView version to install
 [string]$turtl_version="0.7.2.5") #Turtl version to install
 
 ."${PSScriptRoot}/../init_script.ps1"
@@ -74,7 +75,7 @@ else
 Invoke-WebRequest "https://nightly.mpc-hc.org/MPC-HC.${mpchc_version}.x64.exe" -DisableKeepAlive -OutFile "${tempdir}/MPC-HC.exe"
 if (Test-Path "${tempdir}/MPC-HC.exe")
 {"Installing"
-Start-Process "${tempdir}/MPC-HC.exe" -ArgumentList "/closeapplications /mergetasks=`"desktopicon\common`" /nocancel /norestart /restartapplications /silent /sp- /suppressmsgboxes" -Wait #Should use double quotes(") and escape them otherwise backslashes(\) will be broken
+Start-Process "${tempdir}/MPC-HC.exe" -ArgumentList "/closeapplications /mergetasks=`"desktopicon\common`" /nocancel /norestart /restartapplications /silent /sp- /suppressmsgboxes" -Wait #Should use double quotes(") and escape them otherwise tasks inside mergetasks argument will be broken
 "Deleting a temporary file"
 Remove-Item "${tempdir}/MPC-HC.exe" -Force}
 else
@@ -112,6 +113,16 @@ if ($output -like "${tempdir}*")
 else
 {"Cannot download or find Python."}
 
+"Downloading qView"
+Invoke-WebRequest "https://github.com/jurplel/qView/releases/download/${qview_version}/qView-${qview_version}-win64.exe" -DisableKeepAlive -OutFile "${tempdir}/qView.exe"
+if (Test-Path "${tempdir}/qView.exe")
+{"Installing"
+Start-Process "${tempdir}/qView.exe" -ArgumentList "/closeapplications /mergetasks=`"desktopicon`" /nocancel /norestart /restartapplications /silent /sp- /suppressmsgboxes" -Wait #Should use double quotes(") and escape them otherwise tasks inside mergetasks argument will be broken
+"Deleting a temporary file"
+Remove-Item "${tempdir}/qView.exe" -Force}
+else
+{"Cannot download qView."}
+
 "Dowloading Turtl"
 Invoke-WebRequest "https://github.com/turtl/desktop/releases/download/v${turtl_version}/turtl-${turtl_version}-windows64.msi" -DisableKeepAlive -OutFile "${tempdir}/Turtl.msi"
 if (Test-Path "${tempdir}/Turtl.msi")
@@ -126,7 +137,7 @@ else
 Invoke-WebRequest "https://go.microsoft.com/fwlink/?Linkid=852155" -DisableKeepAlive -OutFile "${tempdir}/Visual Studio Code.exe"
 if (Test-Path "${tempdir}/Visual Studio Code.exe")
 {"Installing"
-Start-Process "${tempdir}/Visual Studio Code.exe" -ArgumentList "/closeapplications /mergetasks='addcontextmenufiles,addcontextmenufolders,addtopath,associatewithfiles,desktopicon,!runcode' /nocancel /norestart /restartapplications /silent /sp- /suppressmsgboxes" -Wait
+Start-Process "${tempdir}/Visual Studio Code.exe" -ArgumentList "/closeapplications /mergetasks=`"addcontextmenufiles,addcontextmenufolders,addtopath,associatewithfiles,desktopicon,!runcode`" /nocancel /norestart /restartapplications /silent /sp- /suppressmsgboxes" -Wait #Should use double quotes(") and escape them otherwise tasks inside mergetasks argument will be broken
 "Deleting a temporary file"
 Remove-Item "${tempdir}/Visual Studio Code.exe" -Force}
 else
