ffcac05875570550d1da15f7209ef8c531170d67
PlavorSeol <pseol2190@outlook.com>
2019-06-15 19:54:32 +0900

    Write-Information

--- Windows/uninstall_uwp_apps.ps1
+++ Windows/uninstall_uwp_apps.ps1
@@ -1,50 +1,41 @@
 #Uninstall UWP apps
 #Uninstalls some unnecessary UWP apps that cannot be uninstalled through UI.
 
-."${PSScriptRoot}/../init_script.ps1"
+if (Test-Path "${PSScriptRoot}/init_script.ps1")
+{."${PSScriptRoot}/../init_script.ps1"}
+else
+{"Cannot find initialize script."
+exit}
 
 if (!$IsWindows)
 {"Your operating system is not supported."
 exit}
 
-"Removing Alarms & Clock app"
+Write-Information "Removing Alarms & Clock app"
 Get-AppxPackage "Microsoft.WindowsAlarms" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing Camera app"
+Write-Information "Removing Camera app"
 Get-AppxPackage "Microsoft.WindowsCamera" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing Connect app"
+Write-Information "Removing Connect app"
 Get-AppxPackage "Windows.MiracastView" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing Game bar app"
+Write-Information "Removing Game bar app"
 Get-AppxPackage "Microsoft.XboxGameOverlay" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing Get Help app"
+Write-Information "Removing Get Help app"
 Get-AppxPackage "Microsoft.GetHelp" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing HEIF Image Extensions app"
+Write-Information "Removing HEIF Image Extensions app"
 Get-AppxPackage "Microsoft.HEIFImageExtension" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing Maps app"
+Write-Information "Removing Maps app"
 Get-AppxPackage "Microsoft.WindowsMaps" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing Messaging app"
+Write-Information "Removing Messaging app"
 Get-AppxPackage "Microsoft.Messaging" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing People app"
+Write-Information "Removing People app"
 Get-AppxPackage "Microsoft.People" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing Photos app"
+Write-Information "Removing Photos app"
 Get-AppxPackage "Microsoft.Windows.Photos" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing VP9 Video Extensions app"
+Write-Information "Removing VP9 Video Extensions app"
 Get-AppxPackage "Microsoft.VP9VideoExtensions" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing Webp Image Extensions app"
+Write-Information "Removing Webp Image Extensions app"
 Get-AppxPackage "Microsoft.WebpImageExtension" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing Xbox Game Speech Window app"
+Write-Information "Removing Xbox Game Speech Window app"
 Get-AppxPackage "Microsoft.XboxSpeechToTextOverlay" -AllUsers | Remove-AppxPackage -AllUsers
-
-"Removing Your Phone app"
+Write-Information "Removing Your Phone app"
 Get-AppxPackage "Microsoft.YourPhone" -AllUsers | Remove-AppxPackage -AllUsers
