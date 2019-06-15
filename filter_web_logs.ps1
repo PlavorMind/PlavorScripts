8dc16688eeee8790a191e8e09898a9614e1568f9
PlavorSeol <pseol2190@outlook.com>
2019-06-15 18:26:24 +0900

    Wording

--- filter_web_logs.ps1
+++ filter_web_logs.ps1
@@ -1,8 +1,8 @@
 #Filter web logs
-#Filters web server log files.
+#Filters web server logs.
 
 param
-([string]$destdir="${PSScriptRoot}/filtered_logs", #Destination path to save filtered web server log files
+([string]$destdir="${PSScriptRoot}/filtered_logs", #Destination directory to save filtered web server logs
 [string]$dir="__DEFAULT__") #Web server logs directory to filter
 
 ."${PSScriptRoot}/init_script.ps1"
@@ -25,13 +25,13 @@ if (Test-Path $dir)
 New-Item $destdir -Force -ItemType Directory
 $log_directories=Get-ChildItem $dir -Directory -Force -Name
 foreach ($log_directory in $log_directories)
-  {"Creating a directory for ${log_directory} log directory"
+  {"Creating a directory for ${log_directory} log"
   New-Item "${destdir}/${log_directory}" -Force -ItemType Directory
   $log_files=Get-ChildItem "${dir}/${log_directory}" -File -Force -Name
   foreach ($log_file in $log_files)
     {"Filtering ${log_directory}/${log_file} file"
     Get-Content "${dir}/${log_directory}/${log_file}" -Encoding utf8 -Force | Select-String "(^(127\.0\.0\.[0-9]+|192\.168\.219\.30|::1 ).*|.*GET login\.cgi.*400 150.*)" -Encoding utf8 -NotMatch > "${destdir}/${log_directory}/${log_file}"
-    "Blanking original log file"
+    "Blanking original log"
     #$null > "${dir}/${log_directory}/${log_file}" should not be used because of a bug on Windows.
     Set-Content "${dir}/${log_directory}/${log_file}" $null -Encoding utf8 -Force}
   }
