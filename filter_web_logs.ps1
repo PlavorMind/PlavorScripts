716bafedf167a79edc63c48163ffb8d499bd0fad
PlavorSeol <pseol2190@outlook.com>
2019-06-16 19:44:59 +0900

    Add a check for initialize script

--- filter_web_logs.ps1
+++ filter_web_logs.ps1
@@ -5,7 +5,11 @@ param
 ([string]$destdir="${PSScriptRoot}/filtered_logs", #Destination directory to save filtered web server logs
 [string]$dir="__DEFAULT__") #Web server logs directory to filter
 
-."${PSScriptRoot}/init_script.ps1"
+if (Test-Path "${PSScriptRoot}/init_script.ps1")
+{."${PSScriptRoot}/init_script.ps1"}
+else
+{"Cannot find initialize script."
+exit}
 
 if ($dir -eq "__DEFAULT__")
 {if ($IsLinux)
@@ -21,16 +25,20 @@ if (Test-Path $dir)
 {if (Test-Path $destdir)
   {"Renaming existing directory for filtered logs"
   Move-Item $destdir "${destdir}_old" -Force}
+
 "Creating a directory for filtered logs"
 New-Item $destdir -Force -ItemType Directory
+
 $log_directories=Get-ChildItem $dir -Directory -Force -Name
 foreach ($log_directory in $log_directories)
   {"Creating a directory for ${log_directory} log"
   New-Item "${destdir}/${log_directory}" -Force -ItemType Directory
+
   $log_files=Get-ChildItem "${dir}/${log_directory}" -File -Force -Name
   foreach ($log_file in $log_files)
     {"Filtering ${log_directory}/${log_file} file"
     Get-Content "${dir}/${log_directory}/${log_file}" -Encoding utf8 -Force | Select-String "(^(127\.0\.0\.[0-9]+|192\.168\.219\.30|::1 ).*|.*GET login\.cgi.*400 150.*)" -Encoding utf8 -NotMatch > "${destdir}/${log_directory}/${log_file}"
+
     "Blanking original log"
     #$null > "${dir}/${log_directory}/${log_file}" should not be used because of a bug on Windows.
     Set-Content "${dir}/${log_directory}/${log_file}" $null -Encoding utf8 -Force}
