c48d9bb1698df07200282a9ec5010f04f8be7d40
PlavorSeol <pseol2190@outlook.com>
2019-06-20 21:54:28 +0900

    Add a check for initialize script

--- Windows/PHP/delete_task.ps1
+++ Windows/PHP/delete_task.ps1
@@ -1,7 +1,11 @@
 #Delete PHP CGI/FastCGI autostart task
 #Deletes task to start PHP CGI/FastCGI automatically.
 
-."${PSScriptRoot}/../../init_script.ps1"
+if (Test-Path "${PSScriptRoot}/../../init_script.ps1")
+{."${PSScriptRoot}/../../init_script.ps1"}
+else
+{"Cannot find initialize script."
+exit}
 
 if (!$IsWindows)
 {"Your operating system is not supported."
