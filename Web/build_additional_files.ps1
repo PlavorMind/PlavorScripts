048a8bebad247bf3d558a34786e8b98735a090dc
PlavorSeol <pseol2190@outlook.com>
2019-06-24 03:16:56 +0900

    Create build_additional_files.ps1

---
+++ Web/build_additional_files.ps1
@@ -0,0 +1,49 @@
+#Build additional files
+#Builds additional files for Configure web directory script.
+
+param([string]$dir="__DEFAULT__") #Web server directory
+
+if (Test-Path "${PSScriptRoot}/../init_script.ps1")
+{."${PSScriptRoot}/../init_script.ps1"}
+else
+{"Cannot find initialize script."
+exit}
+
+if ($dir -eq "__DEFAULT__")
+{if ($IsLinux)
+  {$dir="/plavormind/web"}
+elseif ($IsWindows)
+  {$dir="C:/plavormind/web"}
+else
+  {"Cannot detect default directory."
+  exit}
+}
+
+if (Test-Path "${PSScriptRoot}/additional_files")
+{"Renaming existing directory for additional files"
+Move-Item "${PSScriptRoot}/additional_files" "${PSScriptRoot}/additional_files_old" -Force}
+
+"Creating a directory for additioanl files"
+New-Item "${PSScriptRoot}/additional_files" -Force -ItemType Directory
+
+if (Test-Path "${dir}/global")
+{"Creating global directory"
+New-Item "${PSScriptRoot}/additional_files/global" -Force -ItemType Directory
+
+if (Test-Path "${dir}/global/error")
+  {"Creating global/error directory"
+  New-Item "${PSScriptRoot}/additional_files/global/error" -Force -ItemType Directory
+
+  if (Test-Path "${dir}/global/error/2dr3drn_da_vinci_code.jpg")
+    {"Copying global/error/2dr3drn_da_vinci_code.jpg file"
+    Copy-Item "${dir}/global/error/2dr3drn_da_vinci_code.jpg" "${PSScriptRoot}/additional_files/global/error/2dr3drn_da_vinci_code.jpg" -Force}
+
+  if (Test-Path "${dir}/global/error/shinil_yghmrd.jpg")
+    {"Copying global/error/shinil_yghmrd.jpg file"
+    Copy-Item "${dir}/global/error/shinil_yghmrd.jpg" "${PSScriptRoot}/additional_files/global/error/shinil_yghmrd.jpg" -Force}
+  }
+
+if (Test-Path "${dir}/global/favicon.ico")
+  {"Copying global/favicon.ico file"
+  Copy-Item "${dir}/global/favicon.ico" "${PSScriptRoot}/additional_files/global/favicon.ico"}
+}
