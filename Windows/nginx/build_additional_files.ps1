450034fd1e56daf07a824fc9d6568c9c4017b6a3
PlavorSeol <pseol2190@outlook.com>
2019-06-25 03:54:06 +0900

    Update build_additional_files.ps1

--- Windows/nginx/build_additional_files.ps1
+++ Windows/nginx/build_additional_files.ps1
@@ -1,9 +1,13 @@
 #Build additional files
 #Builds additional files for nginx installer script.
 
-param([string]$dir="C:/nginx") #Directory that nginx is installed
+param([string]$dir="C:/plavormind/nginx") #Directory that nginx is installed
 
-."${PSScriptRoot}/../../init_script.ps1"
+if (Test-Path "${PSScriptRoot}/../../init_script.ps1")
+{."${PSScriptRoot}/../../init_script.ps1"}
+else
+{"Cannot find initialize script."
+exit}
 
 if (!$IsWindows)
 {"Your operating system is not supported."
@@ -21,21 +25,3 @@ if (Test-Path "${dir}/conf/private")
 New-Item "${PSScriptRoot}/additional_files/conf" -Force -ItemType Directory
 "Copying conf/private directory"
 Copy-Item "${dir}/conf/private" "${PSScriptRoot}/additional_files/conf/" -Force -Recurse}
-
-if (Test-Path "${dir}/web/main/error/shinil_yghmrd.jpg")
-{"Creating web directory"
-New-Item "${PSScriptRoot}/additional_files/web" -Force -ItemType Directory
-"Creating web/main directory"
-New-Item "${PSScriptRoot}/additional_files/web/main" -Force -ItemType Directory
-"Creating web/main/error directory"
-New-Item "${PSScriptRoot}/additional_files/web/main/error" -Force -ItemType Directory
-"Copying web/main/error/shinil_yghmrd.jpg file"
-Copy-Item "${dir}/web/main/error/shinil_yghmrd.jpg" "${PSScriptRoot}/additional_files/web/main/error/" -Force}
-
-if (Test-Path "${dir}/web/main/favicon.ico")
-{"Creating web directory"
-New-Item "${PSScriptRoot}/additional_files/web" -Force -ItemType Directory
-"Creating web/main directory"
-New-Item "${PSScriptRoot}/additional_files/web/main" -Force -ItemType Directory
-"Copying web/main/favicon.ico file"
-Copy-Item "${dir}/web/main/favicon.ico" "${PSScriptRoot}/additional_files/web/main/" -Force}
