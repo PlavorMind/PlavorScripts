022924539aab8baafaecc3d49e79faee1e0fe60f
PlavorSeol <pseol2190@outlook.com>
2019-06-25 03:51:01 +0900

    Update delete_task.ps1

--- Windows/nginx/delete_task.ps1
+++ Windows/nginx/delete_task.ps1
@@ -1,7 +1,11 @@
 #Delete nginx autostart task
 #Deletes task to start nginx automatically.
 
-."${PSScriptRoot}/../../init_script.ps1"
+if (Test-Path "${PSScriptRoot}/../../init_script.ps1")
+{."${PSScriptRoot}/../../init_script.ps1"}
+else
+{"Cannot find initialize script."
+exit}
 
 if (!$IsWindows)
 {"Your operating system is not supported."
