60d8365ada74786b925a64d4680b9af55b9a9d14
PlavorSeol <pseol2190@outlook.com>
2019-07-01 16:56:58 +0900

    Add check for initialize script

--- Windows/set_windows_features.ps1
+++ Windows/set_windows_features.ps1
@@ -1,7 +1,11 @@
 #Set Windows features
 #Enables or disables some Windows features.
 
-."${PSScriptRoot}/../init_script.ps1"
+if (Test-Path "${PSScriptRoot}/../init_script.ps1")
+{."${PSScriptRoot}/../init_script.ps1"}
+else
+{"Cannot find initialize script."
+exit}
 
 if (!$IsWindows)
 {"Your operating system is not supported."
