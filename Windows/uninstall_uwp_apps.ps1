def1832b073af799562e1ae2cba24ac40d3ae49c
PlavorSeol <pseol2190@outlook.com>
2019-07-01 18:06:43 +0900

    Add check for init_script.ps1

--- Windows/uninstall_uwp_apps.ps1
+++ Windows/uninstall_uwp_apps.ps1
@@ -1,7 +1,11 @@
 #Uninstall UWP apps
 #Uninstalls some unnecessary UWP apps that cannot be uninstalled through UI.
 
-."${PSScriptRoot}/../init_script.ps1"
+if (Test-Path "${PSScriptRoot}/../init_script.ps1")
+{."${PSScriptRoot}/../init_script.ps1"}
+else
+{"Cannot find initialize script."
+exit}
 
 if (!$IsWindows)
 {"Your operating system is not supported."
