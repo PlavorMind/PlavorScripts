9155864f8aa0e46e8e184a88f3f83ab7c3085d2d
PlavorSeol <pseol2190@outlook.com>
2019-07-09 21:40:35 +0900

    Update install.ps1

--- MediaWiki/install.ps1
+++ MediaWiki/install.ps1
@@ -6,7 +6,7 @@ param
 [string]$private_data_dir="__DEFAULT__", #Directory to configure for private data
 [string]$user="PlavorSeol", #User to create during installation and add to the steward group
 [string]$wiki, #Wiki ID
-[string]$wiki_name) #Wiki name
+[string]$wiki_name="") #Wiki name
 
 if (Test-Path "${PSScriptRoot}/../init_script.ps1")
 {."${PSScriptRoot}/../init_script.ps1"}
@@ -34,19 +34,31 @@ else
   exit}
 }
 
-if (Test-Path "${mediawiki_dir}/maintenance/install.php")
-{New-Item "${mediawiki_dir}/data/${wiki}" -Force -ItemType Directory
+if (Test-Path $mediawiki_dir)
+{"Creating data directory"
+New-Item "${mediawiki_dir}/data" -Force -ItemType Directory
+"Creating data directory for ${wiki}"
+New-Item "${mediawiki_dir}/data/${wiki}" -Force -ItemType Directory
+"Creating private data directory"
+New-Item $private_data_dir -Force -ItemType Directory
+"Creating private data directory for ${wiki}"
 New-Item "${private_data_dir}/${wiki}" -Force -ItemType Directory
+"Creating deleted_files directory for ${wiki}"
 New-Item "${private_data_dir}/${wiki}/deleted_files" -Force -ItemType Directory
+"Creating files directory for ${wiki}"
 New-Item "${private_data_dir}/${wiki}/files" -Force -ItemType Directory
 
-if (!(Test-Path "${mediawiki_dir}/data/${wiki}/settings.php"))
-  {"<?php `$wgSitename=`"${wiki_name}`"; ?>">"${mediawiki_dir}/data/${wiki}/settings.php"}
-if (!(Test-Path "${mediawiki_dir}/data/${wiki}/extra_settings.php"))
-  {"<?php ?>">"${mediawiki_dir}/data/${wiki}/extra_settings.php"}
+if (Test-Path "${mediawiki_dir}/LocalSettings.php")
+  {"Renaming LocalSettings.php file temporarily"
+  Move-Item "${mediawiki_dir}/LocalSettings.php" "${mediawiki_dir}/LocalSettings_temp.php" -Force}
 
-php "${mediawiki_dir}/maintenance/install.php" --confpath "${private_data_dir}/${wiki}/LocalSettings.php" --dbname "${wiki}_wiki" --dbpassfile "${PSScriptRoot}/additional_files/database_password.txt" --dbpath "${private_data_dir}/databases" --dbserver "localhost" --dbtype "mysql" --installdbuser "root" --lang "en" --passfile "${PSScriptRoot}/additional_files/user_password.txt" --scriptpath "/mediawiki" $wiki_name $user
+"Running installation script"
+php "${mediawiki_dir}/maintenance/install.php" --confpath "${private_data_dir}/${wiki}" --dbname "${wiki}_wiki" --dbpath "${private_data_dir}/databases" --installdbpass "" --passfile "" $user
 
-."${PSScriptRoot}/init_maintenance.ps1" -dir "${tempdir}/MediaWiki" -steward $user -wiki $wiki}
+if (Test-Path "${mediawiki_dir}/LocalSettings.php")
+  {"Restoring LocalSettings.php file"
+  Move-Item "${mediawiki_dir}/LocalSettings_temp.php" "${mediawiki_dir}/LocalSettings.php" -Force}
+
+."${PSScriptRoot}/init_maintenance.ps1" -dir $mediawiki_dir -steward $user -wiki $wiki}
 else
-{"Cannot find install.php file."}
+{"Cannot find MediaWiki directory."}
