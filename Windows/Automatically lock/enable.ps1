acf3d04e8f59baede43e9df98717c626724f0e11
PlavorSeol <pseol2190@outlook.com>
2019-07-17 22:55:44 +0900

    Add check for init_script.ps1

--- Windows/Automatically lock/enable.ps1
+++ Windows/Automatically lock/enable.ps1
@@ -4,7 +4,11 @@
 
 param([switch]$allusers) #Apply to all users if this parameter is set
 
-."${PSScriptRoot}/../../init_script.ps1"
+if (Test-Path "${PSScriptRoot}/../../init_script.ps1")
+{."${PSScriptRoot}/../../init_script.ps1"}
+else
+{"Cannot find initialize script."
+exit}
 
 if (!$IsWindows)
 {"Your operating system is not supported."
