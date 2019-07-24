7379b33d40693488466b885fae9273f40177b79c
PlavorSeol <pseol2190@outlook.com>
2019-07-24 19:54:47 +0900

    Update branch

--- MediaWiki/init_dir.ps1
+++ MediaWiki/init_dir.ps1
@@ -2,7 +2,7 @@
 #Initializes directories for MediaWiki.
 
 param
-([string]$core_branch="wmf/1.34.0-wmf.14", #Branch for MediaWiki core
+([string]$core_branch="wmf/1.34.0-wmf.15", #Branch for MediaWiki core
 [string]$extra_branch="master", #Branch for extensions and skins
 [string]$mediawiki_dir="__DEFAULT__", #Directory to configure for MediaWiki
 [string]$private_data_dir="__DEFAULT__") #Directory to configure for private data
