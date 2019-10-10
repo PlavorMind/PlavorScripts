22bd0d266800f40822af9bd991897b38ff3d9f9a
PlavorSeol <pseol2190@outlook.com>
2019-10-11 01:33:12 +0900

    Add check for administrator permission

--- Windows/Automatically lock/enable.ps1
+++ Windows/Automatically lock/enable.ps1
@@ -2,7 +2,7 @@
 #Enables automatically lock when signing in.
 #Useful if you use automatic login.
 
-param([switch]$allusers) #Apply to all users if this parameter is set
+Param([switch]$allusers) #Apply to all users if this parameter is set
 
 if (Test-Path "${PSScriptRoot}/../../init_script.ps1")
 {."${PSScriptRoot}/../../init_script.ps1"}
@@ -15,10 +15,12 @@ if (!$IsWindows)
 exit}
 
 if ($allusers)
-{$path="C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk"}
+{if (!(Test-AdminPermission))
+  {"This script must be run as administrator to apply to all users."
+  exit}
+$path="C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk"}
 else
 {$path="${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk"}
 
 "Enabling automatically lock"
-if (!(New-Shortcut -Arguments "user32.dll,LockWorkStation" -Path $path -TargetPath "C:/Windows/System32/rundll32.exe"))
-{"Cannot enable automatically lock."}
+New-Shortcut $path "C:/Windows/System32/rundll32.exe" -Arguments "user32.dll,LockWorkStation"
