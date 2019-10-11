34c92b316c7991ceb83e4b18bd21dca73862d4d4
PlavorSeol <pseol2190@outlook.com>
2019-10-11 17:45:27 +0900

    Define Parameter attribute

--- Windows/PHP/install_data/start.ps1
+++ Windows/PHP/install_data/start.ps1
@@ -1,7 +1,7 @@
 #Start CGI/FastCGI
 #Starts PHP CGI/FastCGI.
 
-param([string]$bind="127.0.0.1:9000") #Bind path for CGI/FastCGI
+Param([Parameter(Position=0)][string]$bind="127.0.0.1:9000") #Bind path for CGI/FastCGI
 
 if (Get-Process "php-cgi" -ErrorAction Ignore)
 {"PHP CGI/FastCGI is already running."}
