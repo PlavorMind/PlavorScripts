ee02b3a3eec4710840b817c044a9a4c758cdf3c0
PlavorSeol <pseol2190@outlook.com>
2019-10-11 17:30:30 +0900

    Define Parameter attribute

--- Windows/nginx/build_additional_files.ps1
+++ Windows/nginx/build_additional_files.ps1
@@ -1,7 +1,7 @@
 #Build additional files
 #Builds additional files for nginx installer script.
 
-param([string]$dir="C:/plavormind/nginx") #Directory that nginx is installed
+Param([Parameter(Position=0)][string]$dir="C:/plavormind/nginx") #Directory that nginx is installed
 
 if (Test-Path "${PSScriptRoot}/../../init_script.ps1")
 {."${PSScriptRoot}/../../init_script.ps1"}
