11a9a8c196eae056037035eb749ed54a0a7272b0
PlavorSeol <pseol2190@outlook.com>
2019-11-07 19:17:04 +0900

    Upgrade PHP to 7.4

--- Windows/PHP/install.ps1
+++ Windows/PHP/install.ps1
@@ -2,9 +2,9 @@
 #Installs PHP with APCu extension.
 
 Param
-([string]$apcu_archive="https://windows.php.net/downloads/pecl/releases/apcu/5.1.17/php_apcu-5.1.17-7.3-nts-vc15-x64.zip", #URL or file path to APCu archive
+([string]$apcu_archive="https://windows.php.net/downloads/pecl/snaps/apcu/5.1.18/php_apcu-5.1.18-7.4-nts-vc15-x64.zip", #URL or file path to APCu archive
 [Parameter(Position=0)][string]$dir="C:/plavormind/php", #Directory to install PHP
-[string]$php_archive="https://windows.php.net/downloads/snaps/php-7.3/r36943df/php-7.3-nts-windows-vc15-x64-r36943df.zip") #URL or file path to PHP archive
+[string]$php_archive="https://windows.php.net/downloads/snaps/php-7.4/rf494a36/php-7.4-nts-windows-vc15-x64-rf494a36.zip") #URL or file path to PHP archive
 
 if (Test-Path "${PSScriptRoot}/../../init_script.ps1")
 {."${PSScriptRoot}/../../init_script.ps1"}
