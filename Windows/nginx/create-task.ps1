6d72be3bbabc23157d9327b2edd25a1ff8323773
PlavorSeol <pseol2190@outlook.com>
2019-11-11 20:19:36 +0900

    Swap order with -ExecutionPolicy and script path
    This commit is to avoid "A parameter cannot be found that matches parameter name 'ExecutionPolicy'." error

--- Windows/nginx/create-task.ps1
+++ Windows/nginx/create-task.ps1
@@ -14,9 +14,9 @@ exit}
 
 "Creating a task"
 if (Test-Path "C:/Program Files/PowerShell/7-preview/pwsh.exe")
-{$action=New-ScheduledTaskAction "C:/Program Files/PowerShell/7-preview/pwsh.exe" "`"${path}`" -ExecutionPolicy Bypass"}
+{$action=New-ScheduledTaskAction "C:/Program Files/PowerShell/7-preview/pwsh.exe" "-ExecutionPolicy Bypass `"${path}`""}
 else
-{$action=New-ScheduledTaskAction "powershell" "`"${path}`" -ExecutionPolicy Bypass"}
+{$action=New-ScheduledTaskAction "powershell" "-ExecutionPolicy Bypass `"${path}`""}
 $principal=New-ScheduledTaskPrincipal "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 $settings=New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Compatibility Win8 -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -ExecutionTimeLimit 0
 $trigger=New-ScheduledTaskTrigger -AtStartup
