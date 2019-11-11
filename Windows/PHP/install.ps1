3f5bf22970bb67b22e926c1a67d3a2a3ab078b6c
PlavorSeol <pseol2190@outlook.com>
2019-11-11 21:09:42 +0900

    Update install.ps1

--- Windows/PHP/install.ps1
+++ Windows/PHP/install.ps1
@@ -1,9 +1,8 @@
-#PHP installer
 #Installs PHP with APCu extension.
 
 Param
 ([string]$apcu_archive="https://windows.php.net/downloads/pecl/snaps/apcu/5.1.18/php_apcu-5.1.18-7.4-nts-vc15-x64.zip", #URL or file path to APCu archive
-[Parameter(Position=0)][string]$dir="C:/plavormind/php", #Directory to install PHP
+[Parameter(Position=0)][string]$dir="C:/plavormind/php-nts", #Directory to install PHP
 [string]$php_archive="https://windows.php.net/downloads/snaps/php-7.4/rf494a36/php-7.4-nts-windows-vc15-x64-rf494a36.zip") #URL or file path to PHP archive
 
 if (Test-Path "${PSScriptRoot}/../../init_script.ps1")
@@ -32,7 +31,7 @@ exit}
 
 $output=Get-FilePathFromUri $php_archive
 if ($output)
-{Expand-Archive $output "${tempdir}/PHP" -Force
+{Expand-Archive $output "${tempdir}/php" -Force
 if ($output -like "${tempdir}*")
   {"Deleting a temporary file"
   Remove-Item $output -Force}
@@ -43,7 +42,7 @@ exit}
 
 $output=Get-FilePathFromUri $apcu_archive
 if ($output)
-{Expand-Archive $output "${tempdir}/APCu" -Force
+{Expand-Archive $output "${tempdir}/apcu" -Force
 if ($output -like "${tempdir}*")
   {"Deleting a temporary file"
   Remove-Item $output -Force}
@@ -52,23 +51,25 @@ else
 {"Cannot download or find APCu archive."
 exit}
 
-"Moving APCu"
-Move-Item "${tempdir}/APCu/php_apcu.dll" "${tempdir}/PHP/ext/" -Force
-"Deleting a temporary directory"
-Remove-Item "${tempdir}/APCu" -Force -Recurse
-
-"Creating data directory"
-New-Item "${tempdir}/PHP/data" -Force -ItemType Directory
-
 "Downloading CA certificate"
-Invoke-WebRequest "https://curl.haxx.se/ca/cacert.pem" -DisableKeepAlive -OutFile "${tempdir}/PHP/data/cacert.pem"
-if (!(Test-Path "${tempdir}/PHP/data/cacert.pem"))
-{"Cannot download CA certificate."
-exit}
+Invoke-WebRequest "https://curl.haxx.se/ca/cacert.pem" -DisableKeepAlive -OutFile "${tempdir}/cacert"
+if (!(Test-Path "${tempdir}/cacert"))
+{"Cannot download CA certificate."}
 
-if (Test-Path "${PSScriptRoot}/../../filter_php_ini.ps1")
-{."${PSScriptRoot}/../../filter_php_ini.ps1" -destpath "${tempdir}/PHP/php.ini"
-if (!(Test-Path "${tempdir}/PHP/php.ini"))
+"Moving APCu"
+Move-Item "${tempdir}/apcu/php_apcu.dll" "${tempdir}/php/ext/" -Force
+"Deleting a temporary directory"
+Remove-Item "${tempdir}/apcu" -Force -Recurse
+
+if (Test-Path "${tempdir}/cacert")
+{"Creating data directory"
+New-Item "${tempdir}/php/data" -Force -ItemType Directory
+"Moving CA certificate"
+Move-Item "${tempdir}/cacert" "${tempdir}/php/data/cacert.pem" -Force}
+
+if (Test-Path "${PSScriptRoot}/../../filter-php-ini.ps1")
+{."${PSScriptRoot}/../../filter-php-ini.ps1" -destpath "${tempdir}/php/php.ini"
+if (!(Test-Path "${tempdir}/php/php.ini"))
   {exit}
 }
 else
@@ -76,23 +77,22 @@ else
 exit}
 
 "Copying install data"
-Copy-Item "${PSScriptRoot}/install_data/start.ps1" "${tempdir}/PHP/" -Force
-Copy-Item "${PSScriptRoot}/install_data/stop.ps1" "${tempdir}/PHP/" -Force
+Copy-Item "${PSScriptRoot}/install-data/start.ps1" "${tempdir}/php/" -Force
+Copy-Item "${PSScriptRoot}/install-data/stop.ps1" "${tempdir}/php/" -Force
 
 "Deleting unnecessary files"
 "Warning: This will remove documentations and license notices that are unnecessary for running."
-Remove-Item "${tempdir}/PHP/license.txt" -Force
-Remove-Item "${tempdir}/PHP/news.txt" -Force
-Remove-Item "${tempdir}/PHP/php.ini-development" -Force
-Remove-Item "${tempdir}/PHP/php.ini-production" -Force
-Remove-Item "${tempdir}/PHP/readme-redist-bins.txt" -Force
-Remove-Item "${tempdir}/PHP/snapshot.txt" -Force
+Remove-Item "${tempdir}/php/license.txt" -Force
+Remove-Item "${tempdir}/php/news.txt" -Force
+Remove-Item "${tempdir}/php/php.ini-development" -Force
+Remove-Item "${tempdir}/php/php.ini-production" -Force
+Remove-Item "${tempdir}/php/readme-redist-bins.txt" -Force
+Remove-Item "${tempdir}/php/snapshot.txt" -Force
 
 "Uninstalling existing PHP"
-."${PSScriptRoot}/uninstall.ps1" -dir $dir
-
+."${PSScriptRoot}/uninstall.ps1" $dir
 "Moving PHP directory"
-Move-Item "${tempdir}/PHP" $dir -Force
+Move-Item "${tempdir}/php" $dir -Force
 
 if (Test-AdminPermission)
-{."${PSScriptRoot}/create_task.ps1" -path "${dir}/start.ps1"}
+{."${PSScriptRoot}/create-task.ps1" "${dir}/start.ps1"}
