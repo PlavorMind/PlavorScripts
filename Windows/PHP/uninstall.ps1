9f17472bd5cb67cbed42f9a73e3a4bbde59e2ce0
PlavorSeol <pseol2190@outlook.com>
2019-12-03 15:12:43 +0900

    Switch to TS build

--- Windows/PHP/uninstall.ps1
+++ Windows/PHP/uninstall.ps1
@@ -1,6 +1,6 @@
 #Uninstalls PHP.
 
-Param([Parameter(Position=0)][string]$dir="C:/plavormind/php-nts") #Directory that PHP is installed
+Param([Parameter(Position=0)][string]$dir="C:/plavormind/php-ts") #Directory that PHP is installed
 
 if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
 {."${PSScriptRoot}/../../init-script.ps1"}
