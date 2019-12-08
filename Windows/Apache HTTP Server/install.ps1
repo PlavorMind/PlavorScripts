8419c109cd7692c4233a8d7d7da8ef50a9e3c530
PlavorSeol <pseol2190@outlook.com>
2019-12-08 18:53:30 +0900

    Add verbose outputs

--- Windows/Apache HTTP Server/install.ps1
+++ Windows/Apache HTTP Server/install.ps1
@@ -16,9 +16,12 @@ if (!$IsWindows)
 {Write-Error "Your operating system is not supported."
 exit}
 
+Write-Verbose "Downloading configurations"
 Invoke-WebRequest "https://github.com/PlavorMind/Configurations/archive/Main.zip" -DisableKeepAlive -OutFile "${tempdir}/config.zip"
 if ("${tempdir}/config.zip")
-{Expand-Archive "${tempdir}/config.zip" $tempdir -Force
+{Write-Verbose "Extracting"
+Expand-Archive "${tempdir}/config.zip" $tempdir -Force
+Write-Verbose "Deleting a file and directory that are no longer needed"
 Remove-Item "${tempdir}/config.zip" -Force
 Move-Item "${tempdir}/Configurations-Main/apache-httpd" "${tempdir}/apache-httpd-config" -Force
 Remove-Item "${tempdir}/Configurations-Main" -Force -Recurse}
@@ -26,15 +29,19 @@ else
 {"Cannot download configurations."
 exit}
 
+Write-Verbose "Downloading Apache HTTP Server"
 Invoke-WebRequest "https://www.apachehaus.com/downloads/httpd-${version}-o111c-x64-vc15-r2.zip" -DisableKeepAlive -OutFile "${tempdir}/apache-httpd.zip"
 if (Test-Path "${tempdir}/apache-httpd.zip")
-{Expand-Archive "${tempdir}/apache-httpd.zip" $tempdir -Force
+{Write-Verbose "Extracting"
+Expand-Archive "${tempdir}/apache-httpd.zip" $tempdir -Force
+Write-Verbose "Deleting files that are no longer needed"
 Remove-Item "${tempdir}/apache-httpd.zip" -Force
 Remove-Item "${tempdir}/readme_first.html" -Force}
 else
 {"Cannot download Apache HTTP Server."
 exit}
 
+Write-Verbose "Applying configurations"
 Move-Item "${tempdir}/apache-httpd-config/*" "${tempdir}/Apache24/conf/" -Force
 Remove-Item "${tempdir}/apache-httpd-config" -Force -Recurse
 Move-Item "${tempdir}/Apache24/conf/os-specific/windows/*" "${tempdir}/Apache24/conf/os-specific/" -Force
@@ -43,21 +50,32 @@ foreach ($os_specific_directory in Get-ChildItem "${tempdir}/Apache24/conf/os-sp
 "Define server_os `"Windows`"" > "${tempdir}/Apache24/conf/os-specific/os.conf"
 
 if (Test-Path $php_dir)
-{Copy-Item "${php_dir}/icudt65.dll" "${tempdir}/Apache24/bin/" -Force
+{Write-Verbose "Copying DLL files from PHP directory"
+Write-Verbose "Some DLL files must be copied from PHP directory to Apache HTTP Server's bin directory to load some PHP extensions."
+Copy-Item "${php_dir}/icudt65.dll" "${tempdir}/Apache24/bin/" -Force
 Copy-Item "${php_dir}/icuin65.dll" "${tempdir}/Apache24/bin/" -Force
 Copy-Item "${php_dir}/icuio65.dll" "${tempdir}/Apache24/bin/" -Force
 Copy-Item "${php_dir}/icuuc65.dll" "${tempdir}/Apache24/bin/" -Force
 Copy-Item "${php_dir}/libssh2.dll" "${tempdir}/Apache24/bin/" -Force
 Copy-Item "${php_dir}/libsqlite3.dll" "${tempdir}/Apache24/bin/" -Force}
+else
+{Write-Warning "Skipped copying DLL files from PHP directory: Cannot find PHP directory."}
 
 if (Test-Path $web_dir)
-{foreach ($virtual_host in Get-ChildItem $web_dir -Directory -Force -Name)
+{Write-Verbose "Creating directories for logs"
+foreach ($virtual_host in Get-ChildItem $web_dir -Directory -Force -Name)
   {New-Item "${tempdir}/Apache24/logs/${virtual_host}" -Force -ItemType Directory}
 }
+else
+{Write-Warning "Skipped creating directories for logs: Cannot find web server public directory."}
 
 if (Test-Path "${PSScriptRoot}/additional-files")
-{Copy-Item "${PSScriptRoot}/additional-files/*" "${tempdir}/Apache24/" -Force -Recurse}
+{Write-Verbose "Copying additional files"
+Copy-Item "${PSScriptRoot}/additional-files/*" "${tempdir}/Apache24/" -Force -Recurse}
+else
+{Write-Warning "Skipped copying additional files: Cannot find additional-files directory."}
 
+Write-Verbose "Deleting files that are unnecessary for running"
 Remove-Item "${tempdir}/Apache24/ABOUT_APACHE.txt" -Force
 Remove-Item "${tempdir}/Apache24/CHANGES.txt" -Force
 Remove-Item "${tempdir}/Apache24/INSTALL.txt" -Force
@@ -69,8 +87,13 @@ Remove-Item "${tempdir}/Apache24/README.txt" -Force
 Remove-Item "${tempdir}/Apache24/logs/install.log" -Force
 
 if (Test-Path $dir)
-{Move-Item $dir "${dir}-old" -Force}
+{Write-Warning "Renaming existing Apache HTTP Server directory"
+Move-Item $dir "${dir}-old" -Force}
+Write-Verbose "Moving Apache HTTP Server directory from temporary directory to destination directory"
 Move-Item "${tempdir}/Apache24" $dir -Force
 
 if (Test-AdminPermission)
-{."${dir}/bin/httpd.exe" -k install}
+{Write-Verbose "Installing service"
+."${dir}/bin/httpd.exe" -k install}
+else
+{Write-Warning "Skipped installing service: This script must be run as administrator to install service."}
