969230a6a7e0de49d0397bf91dcc3ff5d7f4e4ed
PlavorSeol <pseol2190@outlook.com>
2019-12-09 22:10:24 +0900

    /Write-(Error|Verbose|Warning)/

--- filter-web-logs.ps1
+++ filter-web-logs.ps1
@@ -1,8 +1,8 @@
 #Filters web server logs.
 
 Param
-([Parameter(Position=1)][string]$destdir="${PSScriptRoot}/filtered-logs", #Destination directory to save filtered web server logs
-[Parameter(Position=0)][string]$dir) #Web server logs directory to filter
+([Parameter(Position=1)][string]$destdir="${PSScriptRoot}/filtered-logs", #Destination directory to save filtered logs
+[Parameter(Position=0)][string]$dir) #Logs directory to filter
 
 if (Test-Path "${PSScriptRoot}/init-script.ps1")
 {."${PSScriptRoot}/init-script.ps1"}
@@ -11,40 +11,37 @@ else
 exit}
 
 if (!$dir)
-{if ($IsLinux)
-  {$dir="/etc/nginx/logs"}
-elseif ($IsWindows)
-  {$dir="C:/plavormind/nginx/logs"}
+{if ($IsWindows)
+  {$dir="C:/plavormind/apache-httpd/logs"}
 else
   {Write-Error "Cannot detect default directory."
   exit}
 }
 
 if (!(Test-Path $dir))
-{"Cannot find web server logs directory."
+{Write-Error "Cannot find logs directory." -Category ObjectNotFound
 exit}
+#End of preconditions
 
 if (Test-Path $destdir)
-{"Renaming existing directory for filtered logs"
+{Write-Warning "Renaming existing directory for filtered logs"
 Move-Item $destdir "${destdir}-old" -Force}
-"Creating a directory for filtered logs"
+Write-Verbose "Creating a directory for filtered logs"
 New-Item $destdir -Force -ItemType Directory
 
-$log_directories=Get-ChildItem $dir -Directory -Force -Name
-foreach ($log_directory in $log_directories)
-{"Creating ${log_directory} directory"
+foreach ($log_directory in Get-ChildItem $dir -Directory -Force -Name)
+{Write-Verbose "Creating ${log_directory} directory"
 New-Item "${destdir}/${log_directory}" -Force -ItemType Directory}
 
-$log_files=Get-ChildItem $dir -File -Force -Name -Recurse
-foreach ($log_file in $log_files)
-{"Filtering ${log_file} file"
+foreach ($log_file in Get-ChildItem $dir -File -Force -Name -Recurse)
+{Write-Verbose "Filtering ${log_file} file"
 Select-String "(^(127\.0\.0\.[0-9]+|192\.168\.219\.30|::1 ).*|.*GET login\.cgi.*400 150.*)" "${dir}/${log_file}" -NotMatch | Select-Object -ExpandProperty Line > "${destdir}/${log_file}"
 
 if ($log_file -like "*error.log")
-  {"Blanking original file"
+  {Write-Verbose "Blanking original file"
   #$null > "${dir}/${log_file}" should not be used because of a bug on Windows.
   Set-Content "${dir}/${log_file}" $null -Encoding utf8 -Force}
 elseif ($log_file -ne "nginx.pid")
-  {"Deleting original file"
+  {Write-Verbose "Deleting original file"
   Remove-Item "${dir}/${log_file}" -Force}
 }
