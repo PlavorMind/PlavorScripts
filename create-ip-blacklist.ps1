ec11475c94ffe85e39ea975c19aded3f028f09c9
PlavorSeol <pseol2190@outlook.com>
2019-12-10 23:03:11 +0900

    Create create-ip-blacklist.ps1

---
+++ create-ip-blacklist.ps1
@@ -0,0 +1,58 @@
+#Creates an IP address blacklist for specific platform.
+
+Param
+([Parameter(Position=1)][string]$destpath, #Destination path to save created IP address blacklist
+[Parameter(Position=0)][string]$path, #File path or URL to an IP address blacklist source
+[string]$platform="apache-httpd") #Target platform
+
+if (Test-Path "${PSScriptRoot}/init-script.ps1")
+{."${PSScriptRoot}/init-script.ps1"}
+else
+{Write-Error "Cannot find initialize script." -Category ObjectNotFound
+exit}
+
+if (!$destpath)
+{if ($IsWindows)
+  {$destpath="C:/plavormind/apache-httpd/conf/private/blacklist.conf"}
+else
+  {Write-Error "Cannot detect default destination path."
+  exit}
+}
+
+if (!$path)
+{if ($IsLinux)
+  {$path="/home/pseol2190/Documents/blacklist.txt"}
+elseif ($IsWindows)
+  {$path="${Env:USERPROFILE}/OneDrive/Documents/blacklist.txt"}
+else
+  {Write-Error "Cannot detect default path."
+  exit}
+}
+#End of preconditions
+
+$output=Get-FilePathFromUri $path
+if ($output)
+{$source=((Get-Content $output -Force) -replace "#.*","").Trim()
+$blacklist=@()
+foreach ($source_line in $source)
+  {if ($source_line -ne "")
+    {$blacklist+=$source_line}
+  }
+switch ($platform)
+  {"apache-httpd"
+    {"<RequireAll>" > $destpath
+    "Require all granted" >> $destpath
+    $result="Require not ip "
+    foreach ($blacklisted_ip in $blacklist)
+      {$result+="`"${blacklisted_ip}`" "}
+    $result >> $destpath
+    "</RequireAll>" >> $destpath}
+  "nginx"
+    {$null > $destpath
+    foreach ($blacklisted_ip in $blacklist)
+      {"deny `"${blacklisted_ip}`";" >> $destpath}
+    }
+  }
+}
+else
+{Write-Error "Cannot download or find IP address blacklist."}
