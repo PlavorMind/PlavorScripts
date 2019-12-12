dc111ba3b31964fe13d59e161314cfe4aefe7787
PlavorSeol <pseol2190@outlook.com>
2019-12-12 20:50:42 +0900

    Write-Error & Write-Verbose

--- Windows/Automatically lock/enable.ps1
+++ Windows/Automatically lock/enable.ps1
@@ -5,23 +5,23 @@ Param([switch]$allusers) #Apply to all users if this parameter is set
 if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
 {."${PSScriptRoot}/../../init-script.ps1"}
 else
-{"Cannot find initialize script."
+{Write-Error "Cannot find initialize script." -Category ObjectNotFound
 exit}
 
 if (!$IsWindows)
-{"Your operating system is not supported."
+{Write-Error "Your operating system is not supported."
 exit}
 
 if ($allusers)
 {if (!(Test-AdminPermission))
-  {"This script must be run as administrator to apply to all users."
+  {Write-Error "This script must be run as administrator to apply to all users." -Category PermissionDenied
   exit}
 $path="C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk"}
 else
 {$path="${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk"}
 
 if (Test-Path $path)
-{"Automatically lock is already enabled."}
+{Write-Error "Automatically lock is already enabled."}
 else
-{"Enabling automatically lock"
+{Write-Verbose "Enabling automatically lock"
 New-Shortcut $path "C:/Windows/System32/rundll32.exe" -Arguments "user32.dll,LockWorkStation"}
