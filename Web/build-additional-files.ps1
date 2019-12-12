470c1284d2701f0268d8945b9574e6c744d3c33b
PlavorSeol <pseol2190@outlook.com>
2019-12-12 20:17:36 +0900

    Write-Verbose & Write-Warning & Write-Error

--- Web/build-additional-files.ps1
+++ Web/build-additional-files.ps1
@@ -1,34 +1,38 @@
-#Copys additional files for configure-dir.ps1 script.
+#Copys additional files for init-dir.ps1 script.
 
-Param([Parameter(Position=0)][string]$dir) #Web server directory
+Param([Parameter(Position=0)][string]$web_dir) #Web server directory
 
 if (Test-Path "${PSScriptRoot}/../init-script.ps1")
 {."${PSScriptRoot}/../init-script.ps1"}
 else
-{"Cannot find initialize script."
+{Write-Error "Cannot find initialize script." -Category ObjectNotFound
 exit}
 
-if (!$dir)
+if (!$web_dir)
 {if ($IsLinux)
-  {$dir="/plavormind/web"}
+  {$web_dir="/plavormind/web"}
 elseif ($IsWindows)
-  {$dir="C:/plavormind/web"}
+  {$web_dir="C:/plavormind/web"}
 else
-  {"Cannot detect default directory."
+  {Write-Error "Cannot detect default directory." -Category NotSpecified
   exit}
 }
 
+if (!(Test-Path $web_dir))
+{Write-Error "Cannot find web server directory." -Category ObjectNotFound
+exit}
+
 if (Test-Path "${PSScriptRoot}/additional-files")
-{"Renaming existing directory for additional files"
+{Write-Warning "Renaming existing directory for additional files"
 Move-Item "${PSScriptRoot}/additional-files" "${PSScriptRoot}/additional-files-old" -Force}
-"Creating a directory for additioanl files"
+Write-Verbose "Creating a directory for additioanl files"
 New-Item "${PSScriptRoot}/additional-files" -Force -ItemType Directory
 
 if (Test-Path "${dir}/default")
-{"Creating default directory"
+{Write-Verbose "Creating default directory"
 New-Item "${PSScriptRoot}/additional-files/default" -Force -ItemType Directory
 
 if (Test-Path "${dir}/default/favicon.ico")
-  {"Copying default/favicon.ico file"
+  {Write-Verbose "Copying default/favicon.ico file"
   Copy-Item "${dir}/default/favicon.ico" "${PSScriptRoot}/additional-files/default/"}
 }
