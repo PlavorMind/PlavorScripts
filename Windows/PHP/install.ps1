9eff2405a4cd18fdd8120f316fb38e035b936342
PlavorSeol <pseol2190@outlook.com>
2019-12-15 19:22:54 +0900

    Update install.ps1

--- Windows/PHP/install.ps1
+++ Windows/PHP/install.ps1
@@ -1,81 +1,54 @@
 #Installs PHP with some other software that depends on it.
 
 Param
-([string]$apcu_archive="https://windows.php.net/downloads/pecl/snaps/apcu/5.1.18/php_apcu-5.1.18-7.4-ts-vc15-x64.zip", #URL or file path to APCu archive
+([string]$apcu_archive="https://windows.php.net/downloads/pecl/releases/apcu/5.1.18/php_apcu-5.1.18-7.4-ts-vc15-x64.zip", #URL or file path to APCu archive
 [Parameter(Position=0)][string]$dir="C:/plavormind/php-ts", #Directory to install PHP
-[string]$php_archive="https://windows.php.net/downloads/snaps/php-7.4/r996f217/php-7.4-ts-windows-vc15-x64-r996f217.zip") #URL or file path to PHP archive
+[string]$php_archive="https://windows.php.net/downloads/snaps/php-7.4/r7e9e093/php-7.4-ts-windows-vc15-x64-r7e9e093.zip") #URL or file path to PHP archive
 
 if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
 {."${PSScriptRoot}/../../init-script.ps1"}
 else
-{"Cannot find initialize script."
+{Write-Error "Cannot find initialize script." -Category ObjectNotFound
 exit}
 
 if (!$IsWindows)
-{"Your operating system is not supported."
-exit}
-
-#Check processes to avoid errors
-if (Get-Process "php" -ErrorAction Ignore)
-{"PHP is currently running."
-exit}
-if (Get-Process "php-cgi" -ErrorAction Ignore)
-{"PHP CGI/FastCGI is currently running."
-exit}
-if (Get-Process "php-win" -ErrorAction Ignore)
-{"php-win.exe is currently running."
-exit}
-if (Get-Process "phpdbg" -ErrorAction Ignore)
-{"phpdbg.exe is currently running."
+{Write-Error "Your operating system is not supported."
 exit}
 
 $output=Get-FilePathFromUri $php_archive
 if ($output)
-{Expand-Archive $output "${tempdir}/php" -Force
+{Write-Verbose "Extracting PHP"
+Expand-Archive $output "${tempdir}/php" -Force
 if ($output -like "${tempdir}*")
-  {"Deleting a temporary file"
+  {Write-Verbose "Deleting a file that are no longer needed"
   Remove-Item $output -Force}
 }
 else 
-{"Cannot download or find PHP archive."
+{Write-Error "Cannot download or find PHP." -Category ObjectNotFound
 exit}
 
 $output=Get-FilePathFromUri $apcu_archive
 if ($output)
-{Expand-Archive $output "${tempdir}/apcu" -Force
+{Write-Verbose "Extracting APCu"
+Expand-Archive $output "${tempdir}/apcu" -Force
+Write-Verbose "Deleting a file and directory that are no longer needed"
 if ($output -like "${tempdir}*")
-  {"Deleting a temporary file"
-  Remove-Item $output -Force}
-}
+  {Remove-Item $output -Force}
+Move-Item "${tempdir}/apcu/php_apcu.dll" "${tempdir}/php/ext/" -Force
+Remove-Item "${tempdir}/apcu" -Force -Recurse}
 else 
-{"Cannot download or find APCu archive."
+{Write-Error "Cannot download or find APCu." -Category ObjectNotFound
 exit}
 
-"Downloading CA certificate"
+Write-Verbose "Downloading CA certificate"
 Invoke-WebRequest "https://curl.haxx.se/ca/cacert.pem" -DisableKeepAlive -OutFile "${tempdir}/cacert"
 if (!(Test-Path "${tempdir}/cacert"))
-{"Cannot download CA certificate."}
+{Write-Error "Cannot download CA certificate." -Category ConnectionError}
 
-"Downloading Composer"
+Write-Verbose "Downloading Composer"
 Invoke-WebRequest "https://getcomposer.org/composer.phar" -DisableKeepAlive -OutFile "${tempdir}/composer"
 if (!(Test-Path "${tempdir}/composer"))
-{"Cannot download Composer."}
-
-"Moving APCu"
-Move-Item "${tempdir}/apcu/php_apcu.dll" "${tempdir}/php/ext/" -Force
-"Deleting a temporary directory"
-Remove-Item "${tempdir}/apcu" -Force -Recurse
-
-"Creating data directory"
-New-Item "${tempdir}/php/data" -Force -ItemType Directory
-
-if (Test-Path "${tempdir}/cacert")
-{"Moving CA certificate"
-Move-Item "${tempdir}/cacert" "${tempdir}/php/data/cacert.pem" -Force}
-
-if (Test-Path "${tempdir}/composer")
-{"Moving Composer"
-Move-Item "${tempdir}/composer" "${tempdir}/php/data/composer.phar" -Force}
+{Write-Error "Cannot download Composer." -Category ConnectionError}
 
 if (Test-Path "${PSScriptRoot}/../../filter-php-ini.ps1")
 {."${PSScriptRoot}/../../filter-php-ini.ps1" -destpath "${tempdir}/php/php.ini"
@@ -83,16 +56,21 @@ if (!(Test-Path "${tempdir}/php/php.ini"))
   {exit}
 }
 else
-{"Cannot find filter php.ini script."
+{Write-Error "Cannot find filter-php-ini.ps1 script." -Category ObjectNotFound
 exit}
 
-"Copying install data"
+Write-Verbose "Configuring PHP directory"
+New-Item "${tempdir}/php/data" -Force -ItemType Directory
+if (Test-Path "${tempdir}/cacert")
+{Move-Item "${tempdir}/cacert" "${tempdir}/php/data/cacert.pem" -Force}
+if (Test-Path "${tempdir}/composer")
+{Move-Item "${tempdir}/composer" "${tempdir}/php/data/composer.phar" -Force}
+
+Write-Verbose "Copying install data"
 Copy-Item "${PSScriptRoot}/install-data/start.ps1" "${tempdir}/php/" -Force
 Copy-Item "${PSScriptRoot}/install-data/stop.ps1" "${tempdir}/php/" -Force
 
-"Deleting unnecessary files"
-"Warning: This will remove documentations and license notices that are unnecessary for running."
-Remove-Item "${tempdir}/php/logs" -Force -Recurse
+Write-Verbose "Deleting files that are unnecessary for running"
 Remove-Item "${tempdir}/php/license.txt" -Force
 Remove-Item "${tempdir}/php/news.txt" -Force
 Remove-Item "${tempdir}/php/php.ini-development" -Force
@@ -100,11 +78,10 @@ Remove-Item "${tempdir}/php/php.ini-production" -Force
 Remove-Item "${tempdir}/php/README.md" -Force
 Remove-Item "${tempdir}/php/readme-redist-bins.txt" -Force
 Remove-Item "${tempdir}/php/snapshot.txt" -Force
+Remove-Item "${tempdir}/php/logs" -Force -Recurse
 
-"Uninstalling existing PHP"
-."${PSScriptRoot}/uninstall.ps1" $dir
-"Moving PHP directory"
+if (Test-Path $dir)
+{Write-Warning "Uninstalling existing PHP"
+."${PSScriptRoot}/uninstall.ps1" $dir}
+Write-Verbose "Moving PHP directory from temporary directory to destination directory"
 Move-Item "${tempdir}/php" $dir -Force
-
-if (Test-AdminPermission)
-{."${PSScriptRoot}/create-task.ps1" "${dir}/start.ps1"}
