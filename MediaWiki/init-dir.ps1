ff82b4b94336cb95c15c2a4c3e91172d3136e91f
PlavorSeol <pseol2190@outlook.com>
2020-01-09 18:52:27 +0900

    Update init-dir.ps1

--- MediaWiki/init-dir.ps1
+++ MediaWiki/init-dir.ps1
@@ -2,7 +2,7 @@
 
 Param
 ([string]$composer_path, #Path to Composer
-[string]$core_branch="wmf/1.35.0-wmf.10", #Branch for MediaWiki core
+[string]$core_branch="wmf/1.35.0-wmf.14", #Branch for MediaWiki core
 [string]$extra_branch="master", #Branch for extensions and skins
 [Parameter(Position=0)][string]$mediawiki_dir, #Directory to configure for MediaWiki
 [string]$php_path, #Path to PHP
@@ -59,43 +59,43 @@ if (!(Test-Path $php_path))
 {Write-Error "Cannot find PHP." -Category NotInstalled
 exit}
 
-"Downloading Configurations repository archive"
-Invoke-WebRequest "https://github.com/PlavorMind/Configurations/archive/Main.zip" -DisableKeepAlive -OutFile "${tempdir}/Configurations.zip"
-if (Test-Path "${tempdir}/Configurations.zip")
-{"Extracting"
-Expand-Archive "${tempdir}/Configurations.zip" $tempdir -Force
-"Deleting a temporary file"
-Remove-Item "${tempdir}/Configurations.zip" -Force}
+Write-Verbose "Downloading configurations"
+Invoke-WebRequest "https://github.com/PlavorMind/Configurations/archive/Main.zip" -DisableKeepAlive -OutFile "${tempdir}/config.zip"
+if ("${tempdir}/config.zip")
+{Write-Verbose "Extracting"
+Expand-Archive "${tempdir}/config.zip" $tempdir -Force
+Write-Verbose "Deleting a file and directory that are no longer needed"
+Remove-Item "${tempdir}/config.zip" -Force
+Move-Item "${tempdir}/Configurations-Main/MediaWiki" "${tempdir}/mediawiki-config" -Force
+Remove-Item "${tempdir}/Configurations-Main" -Force -Recurse}
 else
-{"Cannot download Configurations repository archive."
+{Write-Error "Cannot download configurations." -Category ConnectionError
 exit}
 
 ."${PSScriptRoot}/download.ps1" "${tempdir}/mw-install" -composer_path $composer_path -core_branch $core_branch -extensions_branch $extra_branch -php_path $php_path -skins_branch $extra_branch
 Move-Item "${tempdir}/mw-install" "${tempdir}/mediawiki" -Force
 
-"Moving configuration files"
-Move-Item "${tempdir}/Configurations-Main/MediaWiki/*" "${tempdir}/mediawiki/" -Force
-"Deleting core cache directory"
+Write-Verbose "Applying configurations"
+Move-Item "${tempdir}/mediawiki-config/*" "${tempdir}/mediawiki/" -Force
+Remove-Item "${tempdir}/mediawiki-config" -Force -Recurse
+Write-Verbose "Deleting cache directory"
 Remove-Item "${tempdir}/mediawiki/cache" -Force -Recurse
-"Deleting core images directory"
+Write-Verbose "Deleting images directory"
 Remove-Item "${tempdir}/mediawiki/images" -Force -Recurse
 
 if (Test-Path "${PSScriptRoot}/additional-files/data")
-{"Copying additional files for data directory"
+{Write-Verbose "Copying additional files for data directory"
 Copy-Item "${PSScriptRoot}/additional-files/data/*" "${tempdir}/mediawiki/data/" -Force -Recurse}
 
 if (Test-Path $mediawiki_dir)
-{"Renaming existing MediaWiki directory"
+{Write-Warning "Renaming existing MediaWiki directory"
 Move-Item $mediawiki_dir "${mediawiki_dir}-old" -Force}
 if (Test-Path $private_data_dir)
-{"Renaming existing private data directory"
+{Write-Warning "Renaming existing private data directory"
 Move-Item $private_data_dir "${private_data_dir}-old" -Force}
-"Moving MediaWiki directory"
+Write-Verbose "Moving MediaWiki directory from temporary directory to destination directory"
 Move-Item "${tempdir}/mediawiki" $mediawiki_dir -Force
 #Copy additional files for private data here because this is just copying a entire directory to seperated location from MediaWiki directory.
 if (Test-Path "${PSScriptRoot}/additional-files/private-data")
-{"Copying additional files for private data directory"
+{Write-Verbose "Copying additional files for private data directory"
 Copy-Item "${PSScriptRoot}/additional-files/private-data" $private_data_dir -Force -Recurse}
-
-"Deleting a temporary directory"
-Remove-Item "${tempdir}/Configurations-Main" -Force -Recurse
