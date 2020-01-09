8dc6771159d7e2d3a59ed925733f74f0f0a0dbfd
PlavorSeol <pseol2190@outlook.com>
2020-01-09 18:19:13 +0900

    Wrtie-Error & Write-Verbose

--- MediaWiki/build-additional-files.ps1
+++ MediaWiki/build-additional-files.ps1
@@ -7,7 +7,7 @@ Param
 if (Test-Path "${PSScriptRoot}/../init-script.ps1")
 {."${PSScriptRoot}/../init-script.ps1"}
 else
-{"Cannot find initialize script."
+{Write-Error "Cannot find initialize script." -Category ObjectNotFound
 exit}
 
 if (!$mediawiki_dir)
@@ -16,7 +16,7 @@ if (!$mediawiki_dir)
 elseif ($IsWindows)
   {$mediawiki_dir="C:/plavormind/web/public/wiki/mediawiki"}
 else
-  {"Cannot detect default directory."
+  {Write-Error "Cannot detect default directory." -Category NotSpecified
   exit}
 }
 
@@ -26,30 +26,29 @@ if (!$private_data_dir)
 elseif ($IsWindows)
   {$private_data_dir="C:/plavormind/web/data/mediawiki"}
 else
-  {"Cannot detect default directory."
+  {Write-Error "Cannot detect default directory." -Category NotSpecified
   exit}
 }
 
 if (Test-Path "${PSScriptRoot}/additional-files")
-{"Renaming existing directory for additional files"
+{Write-Warning "Renaming existing directory for additional files"
 Move-Item "${PSScriptRoot}/additional-files" "${PSScriptRoot}/additional-files-old" -Force}
-"Creating a directory for additioanl files"
+Write-Verbose "Creating a directory for additioanl files"
 New-Item "${PSScriptRoot}/additional-files" -Force -ItemType Directory
 
 if (Test-Path "${mediawiki_dir}/data")
-{"Creating data directory"
+{Write-Verbose "Creating data directory"
 New-Item "${PSScriptRoot}/additional-files/data" -Force -ItemType Directory
 
-$wikis=Get-ChildItem "${mediawiki_dir}/data" -Directory -Force -Name
-foreach ($wiki in $wikis)
+foreach ($wiki in Get-ChildItem "${mediawiki_dir}/data" -Directory -Force -Name)
   {if (Test-Path "${mediawiki_dir}/data/${wiki}/logo.*")
-    {"Creating data/${wiki} directory"
+    {Write-Verbose "Creating data/${wiki} directory"
     New-Item "${PSScriptRoot}/additional-files/data/${wiki}" -Force -ItemType Directory
-    "Copying data/${wiki}/logo.* file"
+    Write-Verbose "Copying data/${wiki}/logo.* file"
     Copy-Item "${mediawiki_dir}/data/${wiki}/logo.*" "${PSScriptRoot}/additional-files/data/${wiki}/" -Force -Recurse}
   }
 }
 
 if (Test-Path $private_data_dir)
-{"Copying private data directory"
+{Write-Verbose "Copying private data directory"
 Copy-Item $private_data_dir "${PSScriptRoot}/additional-files/private-data" -Force -Recurse}
