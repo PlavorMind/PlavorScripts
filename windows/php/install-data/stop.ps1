44b859fd977df1783a29f213049ca66db3cf3d27
PlavorSeol <pseol2190@outlook.com>
2020-01-22 05:15:19 +0900

    Update and rename Windows/PHP/install-data/stop.ps1 to windows/php/install-data/stop.ps1

--- windows/php/install-data/stop.ps1
+++ windows/php/install-data/stop.ps1
@@ -1,8 +1,7 @@
 #Stops PHP CGI/FastCGI.
 
 if (Get-Process "php-cgi" -ErrorAction Ignore)
-{"Stopping PHP CGI/FastCGI"
-$null > "${PSScriptRoot}/stop-php-cgi"
+{Write-Verbose "Stopping PHP CGI/FastCGI"
 Stop-Process -Force -Name "php-cgi"}
 else
-{"PHP CGI/FastCGI is not running."}
+{Write-Error "PHP CGI/FastCGI is not running."}
