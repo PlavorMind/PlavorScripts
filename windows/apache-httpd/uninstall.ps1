23e051ac16c11beca5626a8b5ba24b31284ef572
PlavorSeol <pseol2190@outlook.com>
2020-01-23 02:29:08 +0900

    Add portable mode

--- windows/apache-httpd/uninstall.ps1
+++ windows/apache-httpd/uninstall.ps1
@@ -1,6 +1,8 @@
 #Uninstalls Apache HTTP Server.
 
-Param([Parameter(Position=0)][string]$dir="C:/plavormind/apache-httpd") #Apache HTTP Server directory
+Param
+([Parameter(Position=0)][string]$dir="C:/plavormind/apache-httpd", #Apache HTTP Server directory
+[switch]$portable) #Whether to uninstall in portable mode
 
 if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
 {."${PSScriptRoot}/../../init-script.ps1"}
@@ -12,21 +14,23 @@ if (!$IsWindows)
 {Write-Error "Your operating system is not supported."
 exit}
 
+if (!($portable -or (Test-AdminPermission)))
+{Write-Error "This script must be run as administrator unless you uninstall in portable mode." -Category PermissionDenied
+exit}
+
 if (!(Test-Path $dir))
 {Write-Error "Cannot find Apache HTTP Server." -Category NotInstalled
 exit}
 
-if (Test-AdminPermission)
+if (!$portable)
 {Write-Verbose "Stopping service"
 ."${dir}/bin/httpd.exe" -k stop
 Write-Verbose "Uninstalling service"
 ."${dir}/bin/httpd.exe" -k uninstall}
-else
-{Write-Warning "Skipped uninstalling service: This script must be run as administrator to uninstall service."}
 
 if (Test-Path "${dir}/logs/httpd.pid")
 {$apache_httpd_pid=Get-Content "${dir}/logs/httpd.pid" -Force
-if (Get-Process -Id $apache_httpd_pid)
+if (Get-Process -ErrorAction Ignore -Id $apache_httpd_pid)
   {Write-Verbose "Stopping Apache HTTP Server"
   Stop-Process $apache_httpd_pid
   Start-Sleep 5}
