afff0ccff4f54e6abe25339a8c6722da3705fdb4
PlavorSeol <pseol2190@outlook.com>
2020-01-24 02:59:28 +0900

    Create disable-scheduled-task.ps1

---
+++ windows/php/disable-scheduled-task.ps1
@@ -0,0 +1,17 @@
+#Disables the scheduled task for starting PHP CGI/FastCGI automatically
+
+if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
+{."${PSScriptRoot}/../../init-script.ps1"}
+else
+{Write-Error "Cannot find initialize script." -Category ObjectNotFound
+exit}
+
+if (!(Test-AdminPermission))
+{Write-Error "This script must be run as administrator on Windows." -Category PermissionDenied
+exit}
+
+if (Get-ScheduledTask "PHP CGI FastCGI" -ErrorAction Ignore)
+{Write-Verbose "Disabling the scheduled task"
+Disable-ScheduledTask "PHP CGI FastCGI"}
+else
+{Write-Error "Cannot find the scheduled task." -Category ObjectNotFound}
