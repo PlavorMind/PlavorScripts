21d13bb0df5eeabf1bb29642d3d10a98c8611e55
PlavorSeol <pseol2190@outlook.com>
2020-01-23 18:35:36 +0900

    Add portable mode

--- windows/php/install.ps1
+++ windows/php/install.ps1
@@ -3,7 +3,8 @@
 Param
 ([string]$apcu_archive="https://windows.php.net/downloads/pecl/releases/apcu/5.1.18/php_apcu-5.1.18-7.4-ts-vc15-x64.zip", #URL or file path to APCu archive
 [Parameter(Position=0)][string]$dir="C:/plavormind/php-ts", #Directory to install PHP
-[string]$php_archive="https://windows.php.net/downloads/snaps/php-7.4/ra29c793/php-7.4-ts-windows-vc15-x64-ra29c793.zip") #URL or file path to PHP archive
+[string]$php_archive="https://windows.php.net/downloads/snaps/php-7.4/ra29c793/php-7.4-ts-windows-vc15-x64-ra29c793.zip", #URL or file path to PHP archive
+[switch]$portable) #Whether to install in portable mode
 
 if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
 {."${PSScriptRoot}/../../init-script.ps1"}
@@ -13,6 +14,10 @@ exit}
 
 if (!$IsWindows)
 {Write-Error "Your operating system is not supported."
+exit}
+
+if (!($portable -or (Test-AdminPermission)))
+{Write-Error "This script must be run as administrator unless you install in portable mode." -Category PermissionDenied
 exit}
 
 $output=Get-FilePathFromUri $php_archive
@@ -81,3 +86,14 @@ if (Test-Path $dir)
 ."${PSScriptRoot}/uninstall.ps1" $dir}
 Write-Verbose "Moving PHP directory from temporary directory to destination directory"
 Move-Item "${tempdir}/php" $dir -Force
+
+if (!$portable)
+{Write-Verbose "Creating a scheduled task for starting PHP CGI/FastCGI automatically"
+if (Test-Path "C:/Program Files/PowerShell/7-preview/pwsh.exe")
+  {$action=New-ScheduledTaskAction "C:/Program Files/PowerShell/7-preview/pwsh.exe" "-ExecutionPolicy Bypass `"${path}`""}
+else
+  {$action=New-ScheduledTaskAction "powershell" "-ExecutionPolicy Bypass `"${path}`""}
+$principal=New-ScheduledTaskPrincipal "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+$settings=New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Compatibility Win8 -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -ExecutionTimeLimit 0
+$trigger=New-ScheduledTaskTrigger -AtStartup
+Register-ScheduledTask "PHP-CGI" -Action $action -Description "Start PHP CGI/FastCGI" -Force -Principal $principal -Settings $settings -Trigger $trigger}
