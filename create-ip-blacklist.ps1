bb6000bfce5058d668251c6cac862d14925d4c0b
PlavorSeol <pseol2190@outlook.com>
2020-02-04 07:57:52 +0900

    Support creating a firewall rule

--- create-ip-blacklist.ps1
+++ create-ip-blacklist.ps1
@@ -11,12 +11,35 @@ else
 {Write-Error "Cannot find initialize script." -Category ObjectNotFound
 exit}
 
-if (!$destpath)
-{if ($IsWindows)
-  {$destpath="C:/plavormind/apache-httpd/conf/private/blacklist.conf"}
-else
-  {Write-Error "Cannot detect default destination path." -Category NotSpecified
-  exit}
+switch ($platform)
+{"apache-httpd"
+  {if (!$destpath)
+    {if ($IsWindows)
+      {$destpath="C:/plavormind/apache-httpd/conf/private/blacklist.conf"}
+    else
+      {Write-Error "Cannot detect default destination path." -Category NotSpecified
+      exit}
+    }
+  }
+"firewall"
+  {if ($IsWindows)
+    {if (!(Test-AdminPermission))
+      {Write-Error "This script must be run as administrator to create a firewall rule on Windows." -Category PermissionDenied
+      exit}
+    }
+  else
+    {Write-Error "Your operating system is not supported."
+    exit}
+  }
+"nginx"
+  {if (!$destpath)
+    {if ($IsWindows)
+      {$destpath="C:/plavormind/nginx/conf/private/blacklist.conf"}
+    else
+      {Write-Error "Cannot detect default destination path." -Category NotSpecified
+      exit}
+    }
+  }
 }
 
 $output=Get-FilePathFromUri $path
@@ -33,6 +56,16 @@ switch ($platform)
     foreach ($blacklisted_ip in $blacklist)
       {$result+="`"${blacklisted_ip}`" "}
     $result > $destpath}
+  "firewall"
+    {if ($IsWindows)
+      {if (Get-NetFirewallRule -ErrorAction Ignore -Name "ip-blacklist")
+        {Write-Verbose "Updating existing firewall rule with IP address blacklist source"
+        Set-NetFirewallRule -Name "ip-blacklist" -RemoteAddress $blacklist}
+      else
+        {Write-Verbose "Creating a firewall rule with IP address blacklist source"
+        New-NetFirewallRule -Action Block -Description "Blocks some IP addresses" -DisplayName "IP address blacklist" -Name "ip-blacklist" -RemoteAddress $blacklist}
+      }
+    }
   "nginx"
     {Write-Verbose "Creating IP address blacklist for nginx"
     Out-Null > $destpath
