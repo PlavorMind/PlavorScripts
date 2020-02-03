f1cfb38fd40a7e6d4ab5546bb7f5fed8892ed1fd
PlavorSeol <pseol2190@outlook.com>
2020-02-04 00:03:37 +0900

    Create enable-firewall-rule.ps1

---
+++ windows/apache-httpd/enable-firewall-rule.ps1
@@ -0,0 +1,17 @@
+#Enables the firewall rule for allowing connections to Apache HTTP Server
+
+if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
+{."${PSScriptRoot}/../../init-script.ps1"}
+else
+{Write-Error "Cannot find initialize script." -Category ObjectNotFound
+exit}
+
+if (!(Test-AdminPermission))
+{Write-Error "This script must be run as administrator on Windows." -Category PermissionDenied
+exit}
+
+if (Get-NetFirewallRule -ErrorAction Ignore -Name "apache-httpd")
+{Write-Verbose "Enabling the firewall rule"
+Enable-NetFirewallRule -Name "apache-httpd"}
+else
+{Write-Error "Cannot find the firewall rule." -Category ObjectNotFound}
