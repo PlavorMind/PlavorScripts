4c5a35d761796b95706818fb4fcc90db1524500c
PlavorSeol <pseol2190@outlook.com>
2020-02-06 14:22:29 +0900

    Update stop.ps1

--- windows/php/install-data/stop.ps1
+++ windows/php/install-data/stop.ps1
@@ -1,5 +1,7 @@
 #Stops PHP CGI/FastCGI.
 
+Param([Parameter()]$vb) #Parameter added just for making the -Verbose parameter work and does nothing
+
 if (Get-Process "php-cgi" -ErrorAction Ignore)
 {Write-Verbose "Stopping PHP CGI/FastCGI"
 Stop-Process -Force -Name "php-cgi"}
