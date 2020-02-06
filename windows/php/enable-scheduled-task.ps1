9afbaa5d6f7d6fc37805ab2f86f374c0d1125d74
PlavorSeol <pseol2190@outlook.com>
2020-02-06 14:17:21 +0900

    Update enable-scheduled-task.ps1

--- windows/php/enable-scheduled-task.ps1
+++ windows/php/enable-scheduled-task.ps1
@@ -1,5 +1,7 @@
 #Enables the scheduled task for starting PHP CGI/FastCGI automatically
 
+Param([Parameter()]$vb) #Parameter added just for making the -Verbose parameter work and does nothing
+
 if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
 {."${PSScriptRoot}/../../init-script.ps1"}
 else
