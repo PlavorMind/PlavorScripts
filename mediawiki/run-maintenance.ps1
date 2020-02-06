6b62c3d44fbf177cb792f944228f16f30379a4af
PlavorSeol <pseol2190@outlook.com>
2020-02-06 15:15:06 +0900

    Update run-maintenance.ps1

--- mediawiki/run-maintenance.ps1
+++ mediawiki/run-maintenance.ps1
@@ -1,9 +1,10 @@
 #Runs some MediaWiki maintenance scripts.
 
 Param
-([Parameter(Position=0)][string]$dir, #MediaWiki directory
-[switch]$init, #Run scripts to initialize MediaWiki
+([switch]$init, #Run scripts to initialize MediaWiki
+[Parameter(Position=0)][string]$mediawiki_dir, #MediaWiki directory
 [string]$php_path, #Path to PHP
+[string]$private_data_dir, #Private data directory
 [switch]$update, #Run update.php script
 [string]$wiki) #Specify wiki ID to run scripts otherwise will run globally
 
@@ -13,13 +14,13 @@ else
 {Write-Error "Cannot find initialize script." -Category ObjectNotFound
 exit}
 
-if (!$dir)
+if (!$mediawiki_dir)
 {if ($IsLinux)
-  {$dir="/plavormind/web/public/wiki/mediawiki"}
+  {$mediawiki_dir="/plavormind/web/public/wiki/mediawiki"}
 elseif ($IsWindows)
-  {$dir="C:/plavormind/web/public/wiki/mediawiki"}
+  {$mediawiki_dir="C:/plavormind/web/public/wiki/mediawiki"}
 else
-  {Write-Error "Cannot detect default directory." -Category NotSpecified
+  {Write-Error "Cannot detect default MediaWiki directory." -Category NotSpecified
   exit}
 }
 
@@ -31,32 +32,42 @@ else
   exit}
 }
 
+if (!$private_data_dir)
+{if ($IsLinux)
+  {$private_data_dir="/plavormind/web/data/mediawiki"}
+elseif ($IsWindows)
+  {$private_data_dir="C:/plavormind/web/data/mediawiki"}
+else
+  {Write-Error "Cannot detect default private data directory." -Category NotSpecified
+  exit}
+}
+
 if (!(Test-Path $php_path))
 {Write-Error "Cannot find PHP." -Category NotInstalled
 exit}
 
-if (Test-Path $dir)
+if (Test-Path $mediawiki_dir)
 {if ($wiki)
   {$target_wikis=@($wiki)}
 else
-  {$target_wikis=Get-ChildItem "${dir}/data" -Directory -Force -Name}
+  {$target_wikis=Get-ChildItem "${mediawiki_dir}/data" -Directory -Force -Name}
 foreach ($target_wiki in $target_wikis)
   {if ($init -or $update)
     {Write-Verbose "Running update.php for ${target_wiki}"
-    .$php_path "${dir}/maintenance/update.php" --doshared --quick --wiki $target_wiki}
+    .$php_path "${mediawiki_dir}/maintenance/update.php" --doshared --quick --wiki $target_wiki}
 
   if ($init)
     {Write-Verbose "Running emptyUserGroup.php for ${target_wiki}"
-    .$php_path "${dir}/maintenance/emptyUserGroup.php" "bureaucrat" --wiki $target_wiki
-    .$php_path "${dir}/maintenance/emptyUserGroup.php" "interface-admin" --wiki $target_wiki
-    .$php_path "${dir}/maintenance/emptyUserGroup.php" "sysop" --wiki $target_wiki}
+    .$php_path "${mediawiki_dir}/maintenance/emptyUserGroup.php" "bureaucrat" --wiki $target_wiki
+    .$php_path "${mediawiki_dir}/maintenance/emptyUserGroup.php" "interface-admin" --wiki $target_wiki
+    .$php_path "${mediawiki_dir}/maintenance/emptyUserGroup.php" "sysop" --wiki $target_wiki}
 
   Write-Verbose "Running purgeExpiredUserrights.php for ${target_wiki}"
-  .$php_path "${dir}/maintenance/purgeExpiredUserrights.php" --wiki $target_wiki
+  .$php_path "${mediawiki_dir}/maintenance/purgeExpiredUserrights.php" --wiki $target_wiki
   Write-Verbose "Running pruneFileCache.php for ${target_wiki}"
-  .$php_path "${dir}/maintenance/pruneFileCache.php" --agedays 0 --wiki $target_wiki
+  .$php_path "${mediawiki_dir}/maintenance/pruneFileCache.php" --agedays 0 --wiki $target_wiki
   Write-Verbose "Running runJobs.php for ${target_wiki}"
-  .$php_path "${dir}/maintenance/runJobs.php" --wiki $target_wiki}
+  .$php_path "${mediawiki_dir}/maintenance/runJobs.php" --wiki $target_wiki}
 }
 else
 {Write-Error "Cannot find MediaWiki directory." -Category NotInstalled}
