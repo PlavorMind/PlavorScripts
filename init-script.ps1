b143d218935e39c2324dd1848b8a0f6bda61625f
PlavorSeol <pseol2190@outlook.com>
2020-02-28 06:17:44 +0900

    Do not use + for string concatenation!

--- init-script.ps1
+++ init-script.ps1
@@ -8,16 +8,16 @@ function Expand-ArchiveSmart
 $output=Get-FilePathFromURL $Path
 if ($output)
   {Write-Verbose "Extracting archive"
-  $extracted_items=Expand-Archive $Path $PlaScrTempDirectory+"/expand-archivesmart-extracts/" -Force -PassThru
+  $extracted_items=Expand-Archive $Path "${PlaScrTempDirectory}/expand-archivesmart-extracts/" -Force -PassThru
   if (($extracted_items."Directory" | Measure-Object)."Count" -eq 1)
     {$extracted_directory=$extracted_items."Directory"."Name" | Select-Object -First 1
-    Move-Item $PlaScrTempDirectory+"/expand-archivesmart-extracts/"+$extracted_directory+"/*" $DestinationPath -Force}
+    Move-Item "${PlaScrTempDirectory}/expand-archivesmart-extracts/${extracted_directory}/*" $DestinationPath -Force}
   else
-    {Move-Item $PlaScrTempDirectory+"/expand-archivesmart-extracts/*" $DestinationPath -Force}
+    {Move-Item "${PlaScrTempDirectory}/expand-archivesmart-extracts/*" $DestinationPath -Force}
   Write-Verbose "Deleting a file and a directory that are no longer needed"
-  if ($output -like $PlaScrTempDirectory+"*")
+  if ($output -like "${PlaScrTempDirectory}*")
     {Remove-Item $output -Force}
-  Remove-Item $PlaScrTempDirectory+"/expand-archivesmart-extracts" -Force -Recurse}
+  Remove-Item "${PlaScrTempDirectory}/expand-archivesmart-extracts" -Force -Recurse}
 else
   {Write-Error "Cannot download or find archive."}
 }
@@ -29,16 +29,16 @@ function Get-ConfigFromArchive
 [Parameter(Mandatory=$true,Position=0)][string]$Path)
 
 Write-Verbose "Creating a temporary directory for extracting"
-New-Item $PlaScrTempDirectory+"/get-configfromarchive-config" -Force -ItemType Directory
+New-Item "${PlaScrTempDirectory}/get-configfromarchive-config" -Force -ItemType Directory
 Write-Verbose "Extracting configurations archive"
-Expand-ArchiveSmart $Archive $PlaScrTempDirectory+"/get-configfromarchive-config/"
-if (Test-Path $PlaScrTempDirectory+"/get-configfromarchive-config/"+$Path)
+Expand-ArchiveSmart $Archive "${PlaScrTempDirectory}/get-configfromarchive-config/"
+if (Test-Path "${PlaScrTempDirectory}/get-configfromarchive-config/${Path}")
   {Write-Verbose "Moving configurations"
-  Move-Item $PlaScrTempDirectory+"/get-configfromarchive-config/"+$Path $DestinationPath -Force}
+  Move-Item "${PlaScrTempDirectory}/get-configfromarchive-config/${Path}" $DestinationPath -Force}
 else
   {Write-Error "Cannot download or find configurations."}
 Write-Verbose "Deleting a directory that is no longer needed"
-Remove-Item $PlaScrTempDirectory+"/get-configfromarchive-config" -Force -Recurse}
+Remove-Item "${PlaScrTempDirectory}/get-configfromarchive-config" -Force -Recurse}
 
 #For backward compatibility
 function Get-FilePathFromUri
@@ -55,10 +55,10 @@ if ($URL -match "^https?:\/\/")
   else
     {$filename="get-filepathfromurl"}
   #TODO: Prepend "URL detected" but with the better grammar
-  Write-Verbose "Downloading a file from "+$URL
-  Invoke-WebRequest $URL -DisableKeepAlive -OutFile $PlaScrTempDirectory+"/"+$filename
-  if (Test-Path $PlaScrTempDirectory+"/"+$filename)
-    {return $PlaScrTempDirectory+"/"+$filename}
+  Write-Verbose "Downloading a file from ${URL}"
+  Invoke-WebRequest $URL -DisableKeepAlive -OutFile "${PlaScrTempDirectory}/${filename}"
+  if (Test-Path "${PlaScrTempDirectory}/${filename}")
+    {return "${PlaScrTempDirectory}/${filename}"}
   }
 elseif (Test-Path $URL)
   {return $URL}
@@ -76,7 +76,7 @@ if (Test-Path $TargetPath)
     if ($Arguments)
       {$shortcut.Arguments=$Arguments}
     $shortcut.TargetPath=$TargetPath
-    Write-Verbose "Creating a shortcut to "+$TargetPath+" at "+$Path
+    Write-Verbose "Creating a shortcut to ${TargetPath} at ${Path}"
     $shortcut.Save()}
   else
     {Write-Error "Your operating system is not supported."}
