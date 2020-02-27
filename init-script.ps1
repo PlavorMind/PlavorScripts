3bb70b49070117c9d2fa2d39a1a9eb384804460a
PlavorSeol <pseol2190@outlook.com>
2020-02-28 05:52:41 +0900

    +Expand-ArchiveSmart +Get-ConfigFromArchive, Get-LocalFile->GetFilePathFromURL

--- init-script.ps1
+++ init-script.ps1
@@ -1,20 +1,59 @@
 #Initializes functions, variables, etc. for PlavorScripts.
 
-function Get-FilePathFromUri
+function Expand-ArchiveSmart
 {Param
-([Parameter(Mandatory=$true,Position=0)][string]$Uri)
+([Parameter(Mandatory=$true,Position=1)][string]$DestinationPath,
+[Parameter(Mandatory=$true,Position=0)][string]$Path)
+
+$output=Get-FilePathFromURL $Path
+if ($output)
+  {Write-Verbose "Extracting archive"
+  $extracted_items=Expand-Archive $Path $PlaScrTempDirectory+"/expand-archivesmart-extracts/" -Force -PassThru
+  if (($extracted_items."Directory" | Measure-Object)."Count" -eq 1)
+    {$extracted_directory=$extracted_items."Directory"."Name" | Select-Object -First 1
+    Move-Item $PlaScrTempDirectory+"/expand-archivesmart-extracts/"+$extracted_directory+"/*" $DestinationPath -Force}
+  else
+    {Move-Item $PlaScrTempDirectory+"/expand-archivesmart-extracts/*" $DestinationPath -Force}
+  Write-Verbose "Deleting a file and a directory that are no longer needed"
+  if ($output -like $PlaScrTempDirectory+"*")
+    {Remove-Item $output -Force}
+  Remove-Item $PlaScrTempDirectory+"/expand-archivesmart-extracts" -Force -Recurse}
+else
+  {Write-Error "Cannot download or find archive."}
+}
+
+function Get-ConfigFromArchive
+{Param
+([string]$Archive="https://github.com/PlavorMind/Configurations/archive/master.zip",
+[Parameter(Mandatory=$true,Position=1)][string]$DestinationPath,
+[Parameter(Mandatory=$true,Position=0)][string]$Path)
+
+Write-Verbose "Creating a temporary directory for extracting"
+New-Item $PlaScrTempDirectory+"/get-configfromarchive-config" -Force -ItemType Directory
+Write-Verbose "Extracting configurations archive"
+Expand-ArchiveSmart $Archive $PlaScrTempDirectory+"/get-configfromarchive-config/"
+if (Test-Path $PlaScrTempDirectory+"/get-configfromarchive-config/"+$Path)
+  {Write-Verbose "Moving configurations"
+  Move-Item $PlaScrTempDirectory+"/get-configfromarchive-config/"+$Path $DestinationPath -Force}
+else
+  {Write-Error "Cannot download or find configurations."}
+Write-Verbose "Deleting a directory that is no longer needed"
+Remove-Item $PlaScrTempDirectory+"/get-configfromarchive-config" -Force -Recurse}
 
 #For backward compatibility
-return Get-LocalFile $Uri}
+function Get-FilePathFromUri
+{Param([Parameter(Mandatory=$true,Position=0)][string]$Uri)
 
-function Get-LocalFile
+return Get-FilePathFromURL $Uri}
+
+function Get-FilePathFromURL
 {Param([Parameter(Mandatory=$true,Position=0)][string]$URL)
 
 if ($URL -match "^https?:\/\/")
   {if ($URL -match "[^\\/:*?`"<>|]+\.[^\\/:*?`"<>|]+$")
     {$filename=$Matches[0]}
   else
-    {$filename="get-localfile"}
+    {$filename="get-filepathfromurl"}
   #TODO: Prepend "URL detected" but with the better grammar
   Write-Verbose "Downloading a file from "+$URL
   Invoke-WebRequest $URL -DisableKeepAlive -OutFile $PlaScrTempDirectory+"/"+$filename
