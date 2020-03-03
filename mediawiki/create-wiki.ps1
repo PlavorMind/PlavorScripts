cc74f1834f9a9228188510380d5dc0e7748b77f4
PlavorSeol <pseol2190@outlook.com>
2020-03-03 12:14:46 +0900

    Use JSON file containing credentials, not just passwords

--- mediawiki/create-wiki.ps1
+++ mediawiki/create-wiki.ps1
@@ -1,12 +1,11 @@
 #Creates a wiki.
 
-#Parameter names should not contain "password" to avoid warnings
+#Parameter names should not contain "cred" or "password" to avoid warnings
 Param
-([Parameter(Position=0)][string]$mediawiki_dir, #MediaWiki directory
+([Parameter(Mandatory=$true)][string]$cre_json, #JSON file containing credentials
+[Parameter(Position=0)][string]$mediawiki_dir, #MediaWiki directory
 [string]$php_path, #Path of PHP
 [string]$private_data_dir, #Private data directory
-[Parameter(Mandatory=$true)][string]$pw_json, #JSON file containing passwords
-[Parameter(Mandatory=$true)][string]$user, #Username of the user that will be created during installation
 [Parameter(Mandatory=$true,Position=1)][string]$wiki) #Wiki ID
 
 if (Test-Path "${PSScriptRoot}/../init-script.ps1")
@@ -24,16 +23,18 @@ if (!$php_path)
 if (!$private_data_dir)
 {$private_data_dir="${PlaScrDefaultBaseDirectory}/web/data/mediawiki"}
 
+if (!(Test-Path $cre_json))
+{Write-Error "Cannot find JSON file containing credentials." -Category ObjectNotFound
+exit}
 if (!(Test-Path $php_path))
 {Write-Error "Cannot find PHP." -Category NotInstalled
 exit}
-if (!(Test-Path $pw_json))
-{Write-Error "Cannot find JSON file containing passwords." -Category ObjectNotFound
-exit}
 
-$passwords=Get-Content $pw_json -Force | ConvertFrom-Json
-$database_password=$passwords."database-password"
-$user_password=$passwords."user-password"
+$credentials=Get-Content $cre_json -Force | ConvertFrom-Json
+$database_username=$credentials."database"."username"
+$database_password=$credentials."database"."password"
+$wiki_username=$credentials."wiki"."username"
+$wiki_password=$credentials."wiki"."password"
 
 if (Test-Path $mediawiki_dir)
 {Write-Verbose "Creating data directory"
@@ -54,7 +55,7 @@ if (Test-Path "${mediawiki_dir}/LocalSettings.php")
   Move-Item "${mediawiki_dir}/LocalSettings.php" "${mediawiki_dir}/LocalSettings-temp.php" -Force}
 
 Write-Verbose "Running installation script"
-.$php_path "${mediawiki_dir}/maintenance/install.php" $user --confpath "${private_data_dir}/${wiki}" --dbname "${wiki}wiki" --dbpath "${private_data_dir}/databases" --installdbpass $database_password --installdbuser "root" --pass $user_password
+.$php_path "${mediawiki_dir}/maintenance/install.php" $wiki_username --confpath "${private_data_dir}/${wiki}" --dbname "${wiki}wiki" --dbpath "${private_data_dir}/databases" --installdbpass $database_password --installdbuser $database_username --pass $wiki_password
 
 if (Test-Path "${mediawiki_dir}/LocalSettings-temp.php")
   {Write-Warning "Restoring LocalSettings.php file"
