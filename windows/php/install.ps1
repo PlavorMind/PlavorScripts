7e56ae9f0a04097987ba906da77da952159b4133
PlavorSeol <pseol2190@outlook.com>
2020-03-27 07:22:08 +0900

    Update install.ps1

--- windows/php/install.ps1
+++ windows/php/install.ps1
@@ -1,15 +1,17 @@
 #Installs PHP with some other software depends on it.
 
 Param
-([string]$apcu_archive="https://windows.php.net/downloads/pecl/releases/apcu/5.1.18/php_apcu-5.1.18-7.4-ts-vc15-x64.zip", #URL or file path to APCu archive
+([string]$apcu_archive="https://windows.php.net/downloads/pecl/releases/apcu/5.1.18/php_apcu-5.1.18-7.4-ts-vc15-x64.zip", #File path or URL of APCu archive
 [Parameter(Position=0)][string]$dir="C:/plavormind/php-ts", #Directory to install PHP
-[string]$php_archive="https://windows.php.net/downloads/snaps/php-7.4/rea3afcb/php-7.4-ts-windows-vc15-x64-rea3afcb.zip", #URL or file path to PHP archive
+[string]$php_archive="https://windows.php.net/downloads/snaps/php-7.4/r534e15b/php-7.4-ts-windows-vc15-x64-r534e15b.zip", #File path or URL of PHP archive
 [switch]$portable) #Install in portable mode
 
 if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
-{."${PSScriptRoot}/../../init-script.ps1"}
+{if (!(."${PSScriptRoot}/../../init-script.ps1"))
+  {exit}
+}
 else
-{Write-Error "Cannot find initialize script." -Category ObjectNotFound
+{Write-Error "Cannot find init-script.ps1 file." -Category ObjectNotFound
 exit}
 
 if (!$IsWindows)
@@ -20,72 +22,64 @@ if (!($portable -or (Test-AdminPermission)))
 {Write-Error "This script must be run as administrator unless you install in portable mode." -Category PermissionDenied
 exit}
 
-$output=Get-FilePathFromUri $php_archive
-if ($output)
-{Write-Verbose "Extracting PHP"
-Expand-Archive $output "${tempdir}/php" -Force
-if ($output -like "${tempdir}*")
-  {Write-Verbose "Deleting a file that is no longer needed"
-  Remove-Item $output -Force}
-}
-else 
-{Write-Error "Cannot download or find PHP." -Category ObjectNotFound
-exit}
-
-$output=Get-FilePathFromUri $apcu_archive
-if ($output)
-{Write-Verbose "Extracting APCu"
-Expand-Archive $output "${tempdir}/apcu" -Force
-Write-Verbose "Deleting a file and directory that is no longer needed"
-if ($output -like "${tempdir}*")
-  {Remove-Item $output -Force}
-Move-Item "${tempdir}/apcu/php_apcu.dll" "${tempdir}/php/ext/" -Force
-Remove-Item "${tempdir}/apcu" -Force -Recurse}
-else 
-{Write-Error "Cannot download or find APCu." -Category ObjectNotFound
-exit}
-
 if (Test-Path "${PSScriptRoot}/../../filter-php-ini.ps1")
-{."${PSScriptRoot}/../../filter-php-ini.ps1" -destpath "${tempdir}/php/php.ini"
-if (!(Test-Path "${tempdir}/php/php.ini"))
+{."${PSScriptRoot}/../../filter-php-ini.ps1" -destpath "${PlaScrTempDirectory}/php.ini"
+if (!(Test-Path "${PlaScrTempDirectory}/php.ini"))
   {exit}
 }
 else
-{Write-Error "Cannot find filter-php-ini.ps1 script." -Category ObjectNotFound
+{Write-Error "Cannot find filter-php-ini.ps1 file." -Category ObjectNotFound
 exit}
 
+Expand-ArchiveSmart $php_archive "${PlaScrTempDirectory}/php"
+if (!(Test-Path "${PlaScrTempDirectory}/php"))
+{Write-Error "Cannot download or find PHP." -Category ObjectNotFound
+exit}
+
+Expand-ArchiveSmart $php_archive "${PlaScrTempDirectory}/apcu"
+if (Test-Path "${PlaScrTempDirectory}/apcu")
+{Write-Verbose "Moving APCu extension"
+Move-Item "${PlaScrTempDirectory}/apcu/php_apcu.dll" "${PlaScrTempDirectory}/php/ext/" -Force
+Write-Verbose "Deleting a temporary directory"
+Remove-Item "${PlaScrTempDirectory}/apcu" -Force -Recurse}
+else
+{Write-Error "Cannot download or find APCu extension." -Category ObjectNotFound}
+
+Write-Verbose "Moving php.ini file"
+Move-Item "${PlaScrTempDirectory}/php.ini" "${PlaScrTempDirectory}/php/" -Force
+
 Write-Verbose "Creating data directory"
-New-Item "${tempdir}/php/data" -Force -ItemType Directory
+New-Item "${PlaScrTempDirectory}/php/data" -Force -ItemType Directory
 
 Write-Verbose "Downloading CA certificate"
-Invoke-WebRequest "https://curl.haxx.se/ca/cacert.pem" -DisableKeepAlive -OutFile "${tempdir}/php/data/cacert.pem"
-if (!(Test-Path "${tempdir}/php/data/cacert.pem"))
+Invoke-WebRequest "https://curl.haxx.se/ca/cacert.pem" -DisableKeepAlive -OutFile "${PlaScrTempDirectory}/php/data/cacert.pem"
+if (!(Test-Path "${PlaScrTempDirectory}/php/data/cacert.pem"))
 {Write-Error "Cannot download CA certificate." -Category ConnectionError}
 
 Write-Verbose "Downloading Composer"
-Invoke-WebRequest "https://getcomposer.org/composer.phar" -DisableKeepAlive -OutFile "${tempdir}/php/data/composer.phar"
-if (!(Test-Path "${tempdir}/php/data/composer.phar"))
+Invoke-WebRequest "https://getcomposer.org/composer.phar" -DisableKeepAlive -OutFile "${PlaScrTempDirectory}/php/data/composer.phar"
+if (!(Test-Path "${PlaScrTempDirectory}/php/data/composer.phar"))
 {Write-Error "Cannot download Composer." -Category ConnectionError}
 
 Write-Verbose "Copying install data"
-Copy-Item "${PSScriptRoot}/install-data/start.ps1" "${tempdir}/php/" -Force
-Copy-Item "${PSScriptRoot}/install-data/stop.ps1" "${tempdir}/php/" -Force
+Copy-Item "${PSScriptRoot}/install-data/start.ps1" "${PlaScrTempDirectory}/php/" -Force
+Copy-Item "${PSScriptRoot}/install-data/stop.ps1" "${PlaScrTempDirectory}/php/" -Force
 
-Write-Verbose "Deleting files that are unnecessary for running"
-Remove-Item "${tempdir}/php/license.txt" -Force
-Remove-Item "${tempdir}/php/news.txt" -Force
-Remove-Item "${tempdir}/php/php.ini-development" -Force
-Remove-Item "${tempdir}/php/php.ini-production" -Force
-Remove-Item "${tempdir}/php/README.md" -Force
-Remove-Item "${tempdir}/php/readme-redist-bins.txt" -Force
-Remove-Item "${tempdir}/php/snapshot.txt" -Force
-Remove-Item "${tempdir}/php/logs" -Force -Recurse
+Write-Verbose "Deleting files and a directory that are unnecessary for running"
+Remove-Item "${PlaScrTempDirectory}/php/license.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/php/news.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/php/php.ini-development" -Force
+Remove-Item "${PlaScrTempDirectory}/php/php.ini-production" -Force
+Remove-Item "${PlaScrTempDirectory}/php/README.md" -Force
+Remove-Item "${PlaScrTempDirectory}/php/readme-redist-bins.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/php/snapshot.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/php/logs" -Force -Recurse
 
 if (Test-Path $dir)
 {Write-Warning "Uninstalling existing PHP"
 ."${PSScriptRoot}/uninstall.ps1" $dir}
-Write-Verbose "Moving PHP directory from temporary directory to destination directory"
-Move-Item "${tempdir}/php" $dir -Force
+Write-Verbose "Moving PHP directory to destination directory"
+Move-Item "${PlaScrTempDirectory}/php" $dir -Force
 
 if (!$portable)
 {Write-Verbose "Creating a scheduled task for starting PHP CGI/FastCGI automatically"
