499ef99d223ac4efc2a8d8960c9e39d4f2097c9b
PlavorSeol <pseol2190@outlook.com>
2020-03-27 15:31:33 +0900

    Update install.ps1

--- windows/apache-httpd/install.ps1
+++ windows/apache-httpd/install.ps1
@@ -8,9 +8,11 @@ Param
 [string]$web_dir="C:/plavormind/web/public") #Web public directory
 
 if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
-{."${PSScriptRoot}/../../init-script.ps1"}
+{if (!(."${PSScriptRoot}/../../init-script.ps1"))
+  {exit}
+}
 else
-{Write-Error "Cannot find initialize script." -Category ObjectNotFound
+{Write-Error "Cannot find init-script.ps1 file." -Category ObjectNotFound
 exit}
 
 if (!$IsWindows)
@@ -22,82 +24,75 @@ if (!($portable -or (Test-AdminPermission)))
 exit}
 
 Write-Verbose "Downloading configurations"
-Invoke-WebRequest "https://github.com/PlavorMind/Configurations/archive/master.zip" -DisableKeepAlive -OutFile "${tempdir}/config.zip"
-if ("${tempdir}/config.zip")
-{Write-Verbose "Extracting"
-Expand-Archive "${tempdir}/config.zip" $tempdir -Force
-Write-Verbose "Deleting a file and directory that are no longer needed"
-Remove-Item "${tempdir}/config.zip" -Force
-Move-Item "${tempdir}/Configurations-master/apache-httpd" "${tempdir}/apache-httpd-config" -Force
-Remove-Item "${tempdir}/Configurations-master" -Force -Recurse}
-else
+Get-ItemFromArchive "apache-httpd" "${PlaScrTempDirectory}/apache-httpd-config"
+if (!(Test-Path "${PlaScrTempDirectory}/apache-httpd-config"))
 {Write-Error "Cannot download configurations." -Category ConnectionError
 exit}
 
 Write-Verbose "Creating a temporary directory for extracting"
-New-Item "${tempdir}/apache-httpd-extracts" -Force -ItemType Directory
+New-Item "${PlaScrTempDirectory}/apache-httpd-extracts" -Force -ItemType Directory
 
 Write-Verbose "Downloading Apache HTTP Server"
-#Apache Lounge blocks requests with a user agent string contains name of command line tools such as "curl", "powershell" and "wget". Change user agent if "PlavorScripts" is also blocked.
-Invoke-WebRequest "https://www.apachelounge.com/download/VS16/binaries/httpd-${version}-win64-VS16.zip" -DisableKeepAlive -OutFile "${tempdir}/apache-httpd.zip" -UserAgent "PlavorScripts"
-if (Test-Path "${tempdir}/apache-httpd.zip")
+#Apache Lounge blocks requests with a user agent string contains name of command line tools such as "curl", "powershell" and "wget".
+Invoke-WebRequest "https://www.apachelounge.com/download/VS16/binaries/httpd-${version}-win64-VS16.zip" -DisableKeepAlive -OutFile "${PlaScrTempDirectory}/apache-httpd.zip" -UserAgent "PlavorScripts"
+if (Test-Path "${PlaScrTempDirectory}/apache-httpd.zip")
 {Write-Verbose "Extracting"
-Expand-Archive "${tempdir}/apache-httpd.zip" "${tempdir}/apache-httpd-extracts/" -Force
-Write-Verbose "Deleting a file that is no longer needed"
-Remove-Item "${tempdir}/apache-httpd.zip" -Force
-Move-Item "${tempdir}/apache-httpd-extracts/Apache*" "${tempdir}/apache-httpd" -Force}
+Expand-Archive "${PlaScrTempDirectory}/apache-httpd.zip" "${PlaScrTempDirectory}/apache-httpd-extracts/" -Force
+Write-Verbose "Deleting a temporary file"
+Remove-Item "${PlaScrTempDirectory}/apache-httpd.zip" -Force
+Move-Item "${PlaScrTempDirectory}/apache-httpd-extracts/Apache*" "${PlaScrTempDirectory}/apache-httpd" -Force}
 else
 {Write-Error "Cannot download Apache HTTP Server." -Category ConnectionError
 exit}
 
-Write-Verbose "Deleting a directory that is no longer needed"
-Remove-Item "${tempdir}/apache-httpd-extracts" -Force -Recurse
+Write-Verbose "Deleting a temporary directory"
+Remove-Item "${PlaScrTempDirectory}/apache-httpd-extracts" -Force -Recurse
 
 Write-Verbose "Applying configurations"
-Move-Item "${tempdir}/apache-httpd-config/*" "${tempdir}/apache-httpd/conf/" -Force
-Remove-Item "${tempdir}/apache-httpd-config" -Force -Recurse
-Move-Item "${tempdir}/apache-httpd/conf/os-specific/windows/*" "${tempdir}/apache-httpd/conf/os-specific/" -Force
-foreach ($os_specific_directory in Get-ChildItem "${tempdir}/apache-httpd/conf/os-specific" -Directory -Force -Name)
-{Remove-Item "${tempdir}/apache-httpd/conf/os-specific/${os_specific_directory}" -Force -Recurse}
-"Define server_os `"Windows`"" > "${tempdir}/apache-httpd/conf/os-specific/os.conf"
+Move-Item "${PlaScrTempDirectory}/apache-httpd-config/*" "${PlaScrTempDirectory}/apache-httpd/conf/" -Force
+Remove-Item "${PlaScrTempDirectory}/apache-httpd-config" -Force -Recurse
+Move-Item "${PlaScrTempDirectory}/apache-httpd/conf/os-specific/windows/*" "${PlaScrTempDirectory}/apache-httpd/conf/os-specific/" -Force
+foreach ($os_specific_directory in Get-ChildItem "${PlaScrTempDirectory}/apache-httpd/conf/os-specific" -Directory -Force -Name)
+{Remove-Item "${PlaScrTempDirectory}/apache-httpd/conf/os-specific/${os_specific_directory}" -Force -Recurse}
+"Define server_os `"Windows`"" > "${PlaScrTempDirectory}/apache-httpd/conf/os-specific/os.conf"
 
 if (Test-Path $php_dir)
-{."${PSScriptRoot}/copy-php-dll.ps1" $php_dir "${tempdir}/apache-httpd"}
+{."${PSScriptRoot}/copy-php-dll.ps1" $php_dir "${PlaScrTempDirectory}/apache-httpd"}
 else
 {Write-Warning "Skipped copying DLL files from PHP directory: Cannot find PHP directory."}
 
 if (Test-Path $web_dir)
 {Write-Verbose "Creating directories for logs"
 foreach ($virtual_host in Get-ChildItem $web_dir -Directory -Force -Name)
-  {New-Item "${tempdir}/apache-httpd/logs/${virtual_host}" -Force -ItemType Directory}
+  {New-Item "${PlaScrTempDirectory}/apache-httpd/logs/${virtual_host}" -Force -ItemType Directory}
 }
 else
 {Write-Warning "Skipped creating directories for logs: Cannot find web public directory."}
 
 if (Test-Path "${PSScriptRoot}/additional-files")
 {Write-Verbose "Copying additional files"
-Copy-Item "${PSScriptRoot}/additional-files/*" "${tempdir}/apache-httpd/" -Force -Recurse}
+Copy-Item "${PSScriptRoot}/additional-files/*" "${PlaScrTempDirectory}/apache-httpd/" -Force -Recurse}
 else
 {Write-Warning "Skipped copying additional files: Cannot find additional-files directory."}
 
 Write-Verbose "Deleting files that are unnecessary for running"
-Remove-Item "${tempdir}/apache-httpd/ABOUT_APACHE.txt" -Force
-Remove-Item "${tempdir}/apache-httpd/CHANGES.txt" -Force
-Remove-Item "${tempdir}/apache-httpd/INSTALL.txt" -Force
-Remove-Item "${tempdir}/apache-httpd/LICENSE.txt" -Force
-Remove-Item "${tempdir}/apache-httpd/NOTICE.txt" -Force
-Remove-Item "${tempdir}/apache-httpd/OPENSSL-NEWS.txt" -Force
-Remove-Item "${tempdir}/apache-httpd/OPENSSL-README.txt" -Force
-Remove-Item "${tempdir}/apache-httpd/README.txt" -Force
-Remove-Item "${tempdir}/apache-httpd/logs/install.log" -Force
+Remove-Item "${PlaScrTempDirectory}/apache-httpd/ABOUT_APACHE.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/apache-httpd/CHANGES.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/apache-httpd/INSTALL.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/apache-httpd/LICENSE.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/apache-httpd/NOTICE.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/apache-httpd/OPENSSL-NEWS.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/apache-httpd/OPENSSL-README.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/apache-httpd/README.txt" -Force
+Remove-Item "${PlaScrTempDirectory}/apache-httpd/logs/install.log" -Force
 
 if (Test-Path $dir)
 {Write-Warning "Backing up existing Apache HTTP Server directory"
 Copy-Item $dir "${dir}-old" -Force -Recurse
 Write-Warning "Uninstalling existing Apache HTTP Server"
 ."${PSScriptRoot}/uninstall.ps1" $dir}
-Write-Verbose "Moving Apache HTTP Server directory from temporary directory to destination directory"
-Move-Item "${tempdir}/apache-httpd" $dir -Force
+Write-Verbose "Moving Apache HTTP Server directory to destination directory"
+Move-Item "${PlaScrTempDirectory}/apache-httpd" $dir -Force
 
 if (!$portable)
 {Write-Verbose "Installing service"
