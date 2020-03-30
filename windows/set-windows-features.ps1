c8f8572c26f105b9c1e6a44947dd56ca94cc11a8
PlavorSeol <pseol2190@outlook.com>
2020-03-30 11:27:07 +0900

    Change -vb parameter to -x

--- windows/set-windows-features.ps1
+++ windows/set-windows-features.ps1
@@ -1,6 +1,6 @@
 #Enables or disables some Windows features.
 
-Param([Parameter()]$vb) #Parameter added just for making the -Verbose parameter work and does nothing
+Param([Parameter()]$x) #Parameter added just for making the -Verbose parameter work and does nothing
 
 if (Test-Path "${PSScriptRoot}/../init-script.ps1")
 {if (!(."${PSScriptRoot}/../init-script.ps1"))
