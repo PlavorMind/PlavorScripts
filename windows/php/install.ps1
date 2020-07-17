5f6665230ffb48911663bfe96e272cafd98bb8a7
PlavorSeol <pseol2190@outlook.com>
2020-07-17 10:18:45 +0900

    Update snapshot

--- windows/php/install.ps1
+++ windows/php/install.ps1
@@ -3,7 +3,7 @@
 Param
 ([string]$apcu_archive="https://windows.php.net/downloads/pecl/releases/apcu/5.1.18/php_apcu-5.1.18-7.4-ts-vc15-x64.zip", #File path or URL of APCu archive
 [Parameter(Position=0)][string]$dir="C:/plavormind/php", #Directory to install PHP
-[string]$php_archive="https://windows.php.net/downloads/snaps/php-7.4/r27bb0d9/php-7.4-ts-windows-vc15-x64-r27bb0d9.zip", #File path or URL of PHP archive
+[string]$php_archive="https://windows.php.net/downloads/snaps/php-7.4/r376968c/php-7.4-ts-windows-vc15-x64-r376968c.zip", #File path or URL of PHP archive
 [switch]$portable) #Install in portable mode
 
 if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
