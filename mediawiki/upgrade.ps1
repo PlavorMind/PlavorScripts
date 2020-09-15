1d65154b9b6077e1888486730a3e7bda4dc67b88
PlavorSeol <pseol2190@outlook.com>
2020-09-15 16:36:44 +0900

    Update to reflect new directory structure

--- mediawiki/upgrade.ps1
+++ mediawiki/upgrade.ps1
@@ -3,11 +3,11 @@
 Param
 ([string]$composer_path, #Path of Composer
 [string]$core_branch, #Branch for MediaWiki core
+[string]$data_dir, #Data directory
 [string]$extra_branch="master", #Branch for extensions and skins
-[Parameter(Mandatory=$true)][string]$extras_json, #File path or URL of JSON file for downloading extensions and skins
+[string]$extras_json="${HOME}/OneDrive/Documents/extras.json", #File path or URL of JSON file for downloading extensions and skins
 [Parameter(Position=0)][string]$mediawiki_dir, #MediaWiki directory
-[string]$php_path, #Path of PHP
-[string]$private_data_dir) #Private data directory
+[string]$php_path) #Path of PHP
 
 if (Test-Path "${PSScriptRoot}/../init-script.ps1")
 {if (!(."${PSScriptRoot}/../init-script.ps1"))
@@ -26,12 +26,12 @@ else
   {Write-Error "Cannot detect default Composer path." -Category NotSpecified
   exit}
 }
+if (!$data_dir)
+{$data_dir="${PlaScrDefaultBaseDirectory}/web/data/mediawiki"}
 if (!$mediawiki_dir)
 {$mediawiki_dir="${PlaScrDefaultBaseDirectory}/web/public/wiki/mediawiki"}
 if (!$php_path)
 {$php_path=$PlaScrDefaultPHPPath}
-if (!$private_data_dir)
-{$private_data_dir="${PlaScrDefaultBaseDirectory}/web/data/mediawiki"}
 
 if (!(Test-Path $composer_path))
 {Write-Error "Cannot find Composer." -Category NotInstalled
@@ -61,9 +61,6 @@ else
 if (Test-Path "${mediawiki_dir}/LocalSettings.php")
 {Write-Verbose "Copying existing LocalSettings.php file"
 Copy-Item "${mediawiki_dir}/LocalSettings.php" "${PlaScrTempDirectory}/mediawiki/" -Force}
-if (Test-Path "${mediawiki_dir}/data")
-{Write-Verbose "Copying existing data directory"
-Copy-Item "${mediawiki_dir}/data" "${PlaScrTempDirectory}/mediawiki/" -Force -Recurse}
 Write-Verbose "Deleting cache directory"
 Remove-Item "${PlaScrTempDirectory}/mediawiki/cache" -Force -Recurse
 Write-Verbose "Deleting images directory"
@@ -75,4 +72,4 @@ Move-Item $mediawiki_dir "${mediawiki_dir}-old" -Force}
 Write-Verbose "Moving MediaWiki directory to destination directory"
 Move-Item "${PlaScrTempDirectory}/mediawiki" $mediawiki_dir -Force
 
-."${PSScriptRoot}/maintenance.ps1" $mediawiki_dir -php_path $php_path -private_data_dir $private_data_dir -update
+."${PSScriptRoot}/maintenance.ps1" $mediawiki_dir -data_dir $data_dir -php_path $php_path -update
