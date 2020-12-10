219dc82822f9a6c1f6896cd7b1e56bf23fb93752
PlavorSeol <pseol2190@outlook.com>
2020-12-11 05:49:27 +0900

    Check for existence of firewall rule before deleting or disabling

--- windows/apache-httpd/firewall-rule.ps1
+++ windows/apache-httpd/firewall-rule.ps1
@@ -23,23 +23,28 @@ if (!(Test-AdminPermission))
 {Write-Error "This script must be run as administrator." -Category PermissionDenied
 exit}
 
-switch ($action)
-{"delete"
-  {Write-Verbose "Deleting the firewall rule"
-  Remove-NetFirewallRule -Name "apache-httpd"}
-"disable"
-  {Write-Verbose "Disabling the firewall rule"
-  Disable-NetFirewallRule -Name "apache-httpd"}
-Default
-  {if (Get-NetFirewallRule -ErrorAction Ignore -Name "apache-httpd")
-    {Write-Verbose "Enabling the firewall rule"
-    Enable-NetFirewallRule -Name "apache-httpd"}
-  elseif (Test-Path "${dir}/bin/httpd.exe")
-    {Write-Verbose "Creating a firewall rule"
-    $path="${dir}/bin/httpd.exe".Replace("/","\")
-    New-NetFirewallRule -Action Allow -Description "Allows connections to Apache HTTP Server" -DisplayName "Apache HTTP Server" -Name "apache-httpd" -Program $path}
-  else
-    #This error message should not contain "directory" because the condition checks for httpd.exe file, not Apache HTTP Server directory.
-    {Write-Error "Cannot find Apache HTTP Server." -Category NotInstalled}
+if ($action)
+{if (Get-NetFirewallRule -ErrorAction Ignore -Name "apache-httpd")
+  {switch ($action)
+    {"delete"
+      {Write-Verbose "Deleting the firewall rule"
+      Remove-NetFirewallRule -Name "apache-httpd"}
+    "disable"
+      {Write-Verbose "Disabling the firewall rule"
+      Disable-NetFirewallRule -Name "apache-httpd"}
+    }
   }
+else
+  {Write-Error "Cannot find the firewall rule." -Category ObjectNotFound}
+}
+else
+{if (Get-NetFirewallRule -ErrorAction Ignore -Name "apache-httpd")
+  {Write-Verbose "Enabling the firewall rule"
+  Enable-NetFirewallRule -Name "apache-httpd"}
+elseif (Test-Path "${dir}/bin/httpd.exe")
+  {Write-Verbose "Creating a firewall rule"
+  $path="${dir}/bin/httpd.exe".Replace("/","\")
+  New-NetFirewallRule -Action Allow -Description "Allows connections to Apache HTTP Server" -DisplayName "Apache HTTP Server" -Name "apache-httpd" -Program $path}
+else
+  {Write-Error "Cannot find Apache HTTP Server." -Category NotInstalled}
 }
