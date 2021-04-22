8830e62ade9446d077bbba851d39739c54fce311
PlavorSeol <pseol2190@outlook.com>
2021-04-23 06:55:10 +0900

    Update common-functions.ps1

--- src/common-functions.ps1
+++ src/common-functions.ps1
@@ -1,29 +1,45 @@
-# Creates a shortcut.
-# This function only supports Windows.
+<#
+.description
+Downloads a file from specified URL.
+#>
+function Get-FileFromURL
+  {param
+  ([Parameter(Mandatory=$true, Position=1)][string]$Path,
+  [Parameter(Mandatory=$true, Position=0)][string]$URL)
+
+  $ProgressPreference_temp=$ProgressPreference
+  $ProgressPreference='SilentlyContinue'
+  Invoke-WebRequest $URL -MaximumRetryCount 2 -OutFile $Path -RetryIntervalSec 3
+  $ProgressPreference=$ProgressPreference_temp}
+
+<#
+.description
+Creates a shortcut. This function only supports Windows.
+#>
 function New-Shortcut
   {param
   # Parameters to use when using the shortcut
-  ([Parameter(Position=2)][string]$Arguments,
+  ([Parameter(Position=2)][string]$Parameters,
   # Shourtcut path
   [Parameter(Mandatory=$true, Position=0)][string]$Path,
   # Target of the shortcut
-  [Parameter(Mandatory=$true, Position=1)][string]$TargetPath)
+  [Parameter(Mandatory=$true, Position=1)][string]$Target)
 
   # Check requirements
   if (!$IsWindows)
     {Write-Error 'New-Shortcut does not support operating systems other than Windows.'
     return}
 
-  if (Test-Path $TargetPath)
+  if (Test-Path $Target)
     # -ComObject parameter must be specified otherwise New-Object will throw an error in newer PowerShell versions.
     {$shortcut=(New-Object -ComObject 'WScript.Shell').CreateShortcut($Path)
-    $shortcut.TargetPath=$TargetPath
+    $shortcut.TargetPath=$Target
 
-    if ($Arguments)
-      {$shortcut.Arguments=$Arguments
-      $target_display="${TargetPath} ${Arguments}"}
+    if ($Parameters)
+      {$shortcut.Arguments=$Parameters
+      $target_display="${Target} ${Parameters}"}
     else
-      {$target_display=$TargetPath}
+      {$target_display=$Target}
 
     Write-Verbose "Creating a shortcut to ${target_display} at ${Path}"
     $shortcut.Save()}
@@ -31,7 +47,10 @@ function New-Shortcut
     {Write-Error 'Cannot find the target.' -Category ResourceUnavailable}
   }
 
-# Returns if the user has administrator permission on Windows, or root permission on Linux.
+<#
+.description
+Returns if the user has administrator permission on Windows, or root permission on Linux.
+#>
 function Test-AdminPermission
   {if ($IsLinux)
     {return (id --user) -eq 0}
