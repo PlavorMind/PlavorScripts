547849b202325ac957cc93800f2c895e9bff85d0
PlavorSeol <pseol2190@outlook.com>
2021-04-25 22:25:40 +0900

    Update common-functions.ps1

--- src/common-functions.ps1
+++ src/common-functions.ps1
@@ -4,13 +4,16 @@ Downloads a file from specified URL.
 #>
 function Get-FileFromURL
   {param
-  ([Parameter(Mandatory=$true, Position=1)][string]$Path,
-  [Parameter(Mandatory=$true, Position=0)][string]$URL)
+  # Path to save downloaded file
+  ([Parameter(Mandatory = $true, Position = 1)][string]$Path,
+  # URL to download a file
+  [Parameter(Mandatory = $true, Position = 0)][string]$URL)
 
-  $ProgressPreference_temp=$ProgressPreference
-  $ProgressPreference='SilentlyContinue'
+  $ProgressPreference_temp = $ProgressPreference
+  $ProgressPreference = 'SilentlyContinue'
+  Write-Verbose "Downloading a file from ${URL}"
   Invoke-WebRequest $URL -MaximumRetryCount 2 -OutFile $Path -RetryIntervalSec 3
-  $ProgressPreference=$ProgressPreference_temp}
+  $ProgressPreference = $ProgressPreference_temp}
 
 <#
 .description
@@ -19,33 +22,33 @@ Creates a shortcut. This function only supports Windows.
 function New-Shortcut
   {param
   # Parameters to use when using the shortcut
-  ([Parameter(Position=2)][string]$Parameters,
+  ([Parameter(Position = 2)][string]$Parameters,
   # Shourtcut path
-  [Parameter(Mandatory=$true, Position=0)][string]$Path,
+  [Parameter(Mandatory = $true, Position = 0)][string]$Path,
   # Target of the shortcut
-  [Parameter(Mandatory=$true, Position=1)][string]$Target)
+  [Parameter(Mandatory = $true, Position = 1)][string]$Target)
 
   # Check requirements
   if (!$IsWindows)
     {Write-Error 'New-Shortcut does not support operating systems other than Windows.'
     return}
 
-  if (Test-Path $Target)
-    # -ComObject parameter must be specified otherwise New-Object will throw an error in newer PowerShell versions.
-    {$shortcut=(New-Object -ComObject 'WScript.Shell').CreateShortcut($Path)
-    $shortcut.TargetPath=$Target
+  if (!(Test-Path $Target))
+    {Write-Error 'Cannot find the target.' -Category ObjectNotFound
+    return}
 
-    if ($Parameters)
-      {$shortcut.Arguments=$Parameters
-      $target_display="${Target} ${Parameters}"}
-    else
-      {$target_display=$Target}
+  # -ComObject parameter must be specified otherwise New-Object will throw an error in newer PowerShell versions.
+  $shortcut = (New-Object -ComObject 'WScript.Shell').CreateShortcut($Path)
+  $shortcut.TargetPath = $Target
 
-    Write-Verbose "Creating a shortcut to ${target_display} at ${Path}"
-    $shortcut.Save()}
+  if ($Parameters)
+    {$shortcut.Arguments = $Parameters
+    $target_display = "${Target} ${Parameters}"}
   else
-    {Write-Error 'Cannot find the target.' -Category ResourceUnavailable}
-  }
+    {$target_display = $Target}
+
+  Write-Verbose "Creating a shortcut to ${target_display} at ${Path}"
+  $shortcut.Save()}
 
 <#
 .description
@@ -55,7 +58,7 @@ function Test-AdminPermission
   {if ($IsLinux)
     {return (id --user) -eq 0}
   elseif ($IsWindows)
-    {$identity=[Security.Principal.WindowsIdentity]::GetCurrent()
-    $principal=New-Object Security.Principal.WindowsPrincipal($identity)
+    {$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal($identity)
     return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)}
   }
