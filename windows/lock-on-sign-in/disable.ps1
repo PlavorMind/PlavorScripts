a3c8a744502a0908a309402c939b8bdbb6d98bae
PlavorSeol <pseol2190@outlook.com>
2021-09-03 00:09:45 +0900

    Apply new coding conventions

--- windows/lock-on-sign-in/disable.ps1
+++ windows/lock-on-sign-in/disable.ps1
@@ -1,16 +1,20 @@
 # Disables automatically locking on sign in.
 
-if (Test-Path "${PSScriptRoot}/../../init-script.ps1")
-  {."${PSScriptRoot}/../../init-script.ps1" | Out-Null}
-else
-  {throw 'Cannot find init-script.ps1 file.'}
+if (Test-Path "${PSScriptRoot}/../../init-script.ps1") {
+  ."${PSScriptRoot}/../../init-script.ps1" | Out-Null
+  }
+else {
+  throw 'Cannot find init-script.ps1 file.'
+  }
 
-# Check requirements
-if (!$IsWindows)
-  {throw 'This script does not support operating systems other than Windows.'}
+if (!$IsWindows) {
+  throw 'This script does not support operating systems other than Windows.'
+  }
 
-if (Test-Path "${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk")
-  {'Disabling automatically locking on sign in'
-  Remove-Item "${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk" -Force}
-else
-  {Write-Error 'Automatically locking on sign in is not enabled.' -Category NotEnabled}
+if (Test-Path "${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk") {
+  'Disabling automatically locking on sign in'
+  Remove-Item "${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk" -Force
+  }
+else {
+  Write-Error 'Automatically locking on sign in is not enabled.' -Category NotEnabled
+  }
