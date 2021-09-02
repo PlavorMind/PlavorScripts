ad3a6ba5380e68ceaff16918af91b758e2afc39a
PlavorSeol <pseol2190@outlook.com>
2021-09-02 18:30:14 +0900

    Drop support for 32-bit operating systems
    Also apply new coding conventions

--- init-script.ps1
+++ init-script.ps1
@@ -1,22 +1,31 @@
-if ($PSVersionTable.PSVersion.Major -lt 7)
-  {throw 'PlavorScripts does not support PowerShell 6 or older.'}
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+  throw 'PlavorScripts does not support PowerShell 6 or older.'
+  }
 
-if ($IsMacOS)
-  {throw 'PlavorScripts does not support macOS.'}
-elseif ($IsWindows -and ([System.Environment]::OSVersion.Version.Major -lt 10))
-  {throw 'PlavorScripts does not support Windows NT 6.3 (Windows 8.1) or older.'}
+if ($IsMacOS) {
+  throw 'PlavorScripts does not support macOS.'
+  }
+elseif ($IsWindows -and ([Environment]::OSVersion.Version.Major -lt 10)) {
+  throw 'PlavorScripts does not support Windows NT 6.3 (Windows 8.1) or older.'
+  }
+
+if (![Environment]::Is64BitOperatingSystem) {
+  throw 'PlavorScripts does not support 32-bit operating systems.'
+  }
 
 # Initialize variables
 $PlaScrDirectory = $PSScriptRoot
 
-if ($IsLinux)
-  {$PlaScrDefaultBaseDirectory = '/plavormind'
+if ($IsLinux) {
+  $PlaScrDefaultBaseDirectory = '/plavormind'
   $PlaScrDefaultPHPPath = '/usr/bin/php'
-  $PlaScrTempDirectory = '/tmp'}
-elseif ($IsWindows)
-  {$PlaScrDefaultBaseDirectory = 'C:/plavormind'
+  $PlaScrTempDirectory = '/tmp'
+  }
+elseif ($IsWindows) {
+  $PlaScrDefaultBaseDirectory = 'C:/plavormind'
   $PlaScrDefaultPHPPath = "${PlaScrDefaultBaseDirectory}/php/php.exe"
-  $PlaScrTempDirectory = $Env:TEMP}
+  $PlaScrTempDirectory = $Env:TEMP
+  }
 
 # Initialize environment variables
 $Env:POWERSHELL_TELEMETRY_OPTOUT = 1
