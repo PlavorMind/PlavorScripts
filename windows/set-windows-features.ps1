1d0a44c7508b12bf78ad9e5d47dfd64459a90ae7
PlavorSeol <pseol2190@outlook.com>
2021-09-03 19:14:54 +0900

    Update set-windows-features.ps1

--- windows/set-windows-features.ps1
+++ windows/set-windows-features.ps1
@@ -1,48 +1,76 @@
-#Enables or disables some Windows features.
+# Enables and disables Windows features.
 
-Param([Parameter()]$x) #Parameter added just for making the -Verbose parameter work and does nothing
+param (
+  # Parameter added just for making the -Verbose parameter work and does nothing
+  [Parameter()]$x
+  )
 
-if (Test-Path "${PSScriptRoot}/../init-script.ps1")
-{if (!(."${PSScriptRoot}/../init-script.ps1"))
-  {exit}
-}
-else
-{Write-Error "Cannot find init-script.ps1 file." -Category ObjectNotFound
-exit}
+if (Test-Path "${PSScriptRoot}/../init-script.ps1") {
+  ."${PSScriptRoot}/../init-script.ps1" | Out-Null
+  }
+else {
+  throw 'Cannot find init-script.ps1 file.'
+  }
 
-if (!(Test-AdminPermission))
-{Write-Error "This script must be run as administrator on Windows." -Category PermissionDenied
-exit}
+if (!$IsWindows) {
+  throw 'This script does not support operating systems other than Windows.'
+  }
 
-Write-Verbose "Disabling Media Features (root) feature"
-Disable-WindowsOptionalFeature -FeatureName "MediaPlayback" -NoRestart -Online
+if (!(Test-AdminPermission)) {
+  throw 'This script requires administrator permission.'
+  }
 
-Write-Verbose "Disabling Media Features/Windows Media Player feature"
-Disable-WindowsOptionalFeature -FeatureName "WindowsMediaPlayer" -NoRestart -Online
+# This is actually a hashtable and not PSCustomObject, but it works.
+$settings_object = @{
+  disable = @(
+    'MediaPlayback',
+    'MicrosoftWindowsPowerShellV2',
+    'MicrosoftWindowsPowerShellV2Root',
+    'MSRDC-Infrastructure',
+    'Printing-Foundation-Features',
+    'Printing-XPSServices-Features',
+    'SmbDirect',
+    'WindowsMediaPlayer',
+    'WorkFolders-Client'
+    )
+  enable = @(
+    'Microsoft-Windows-Subsystem-Linux'
+    )
+  }
 
-Write-Verbose "Disabling Microsoft XPS Document Writer feature"
-Disable-WindowsOptionalFeature -FeatureName "Printing-XPSServices-Features" -NoRestart -Online
+$all_features_object = Get-WindowsOptionalFeature -Online
 
-Write-Verbose "Disabling Print and Document Services (root) feature"
-Disable-WindowsOptionalFeature -FeatureName "Printing-Foundation-Features" -NoRestart -Online
+foreach ($action in 'disable', 'enable') {
+  $features = $settings_object.$action
 
-Write-Verbose "Disabling Print and Document Services/Internet Printing Client feature"
-Disable-WindowsOptionalFeature -FeatureName "Printing-Foundation-InternetPrinting-Client" -NoRestart -Online
+  foreach ($feature in $features) {
+    if ($feature -notin $all_features_object.FeatureName) {
+      Write-Error "${feature} is not a valid feature." -Category InvalidData
+      continue
+      }
 
-Write-Verbose "Disabling Remote Differential Compression API Support feature"
-Disable-WindowsOptionalFeature -FeatureName "MSRDC-Infrastructure" -NoRestart -Online
+    $detailed_feature_object = Get-WindowsOptionalFeature -FeatureName $feature -Online
+    $feature_display_name = $detailed_feature_object.DisplayName
 
-Write-Verbose "Disabling SMB Direct feature"
-Disable-WindowsOptionalFeature -FeatureName "SmbDirect" -NoRestart -Online
-
-Write-Verbose "Disabling Windows PowerShell 2.0 (root) feature"
-Disable-WindowsOptionalFeature -FeatureName "MicrosoftWindowsPowerShellV2Root" -NoRestart -Online
-
-Write-Verbose "Disabling Windows PowerShell 2.0/Windows PowerShell 2.0 Engine feature"
-Disable-WindowsOptionalFeature -FeatureName "MicrosoftWindowsPowerShellV2" -NoRestart -Online
-
-Write-Verbose "Enabling Windows Subsystem for Linux feature"
-Enable-WindowsOptionalFeature -FeatureName "Microsoft-Windows-Subsystem-Linux" -NoRestart -Online
-
-Write-Verbose "Disabling Work Folders Client feature"
-Disable-WindowsOptionalFeature -FeatureName "WorkFolders-Client" -NoRestart -Online
+    switch ($action) {
+      'disable' {
+        if ($detailed_feature_object.State -eq 'Disabled') {
+          Write-Warning "${feature_display_name} (${feature}) feature is already disabled."
+          }
+        else {
+          "Disabling ${feature_display_name} (${feature}) feature"
+          Disable-WindowsOptionalFeature -FeatureName $feature -NoRestart -Online
+          }
+        }
+      'enable' {
+        if ($detailed_feature_object.State -eq 'Enabled') {
+          Write-Warning "${feature_display_name} (${feature}) feature is already enabled."
+          }
+        else {
+          "Enabling ${feature_display_name} (${feature}) feature"
+          Enable-WindowsOptionalFeature -FeatureName $feature -NoRestart -Online
+          }
+        }
+      }
+    }
+  }
