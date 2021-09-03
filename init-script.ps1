053f1f3ceb66d7a0accb52cc04b0193e859f6bd4
PlavorSeol <pseol2190@outlook.com>
2021-09-03 19:32:39 +0900

    Use Temp: PSDrive for $PlaScrTempDirectory

--- init-script.ps1
+++ init-script.ps1
@@ -24,7 +24,7 @@ if ($IsLinux) {
 elseif ($IsWindows) {
   $PlaScrDefaultBaseDirectory = 'C:/plavormind'
   $PlaScrDefaultPHPPath = "${PlaScrDefaultBaseDirectory}/php/php.exe"
-  $PlaScrTempDirectory = $Env:TEMP
+  $PlaScrTempDirectory = 'Temp:'
   }
 
 # Initialize environment variables
