50b41e4de67cf508360a1625e92b2cb790c01652
PlavorSeol <pseol2190@outlook.com>
2022-02-03 22:01:12 +0900

    Apply new coding conventions

--- windows/lock-on-sign-in/disable.ps1
+++ windows/lock-on-sign-in/disable.ps1
@@ -2,19 +2,19 @@
 
 if (Test-Path "${PSScriptRoot}/../../init-script.ps1") {
   ."${PSScriptRoot}/../../init-script.ps1" | Out-Null
-  }
+}
 else {
   throw 'Cannot find init-script.ps1 file.'
-  }
+}
 
 if (!$IsWindows) {
   throw 'This script does not support operating systems other than Windows.'
-  }
+}
 
 if (Test-Path "${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk") {
   'Disabling automatically locking on sign in'
   Remove-Item "${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk" -Force
-  }
+}
 else {
   Write-Error 'Automatically locking on sign in is not enabled.' -Category NotEnabled
-  }
+}
