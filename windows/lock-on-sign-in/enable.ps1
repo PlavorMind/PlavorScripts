b8f5314790750ef3a5abad8933c5fabbb933b60d
PlavorSeol <pseol2190@outlook.com>
2022-03-06 21:37:48 +0900

    Apply new coding conventions

--- windows/lock-on-sign-in/enable.ps1
+++ windows/lock-on-sign-in/enable.ps1
@@ -1,7 +1,7 @@
 # Enables automatically locking on sign in. Useful when using automatic login.
 
-if (Test-Path "${PSScriptRoot}/../../init-script.ps1") {
-  ."${PSScriptRoot}/../../init-script.ps1" | Out-Null
+if (Test-Path "$PSScriptRoot/../../init-script.ps1") {
+  ."$PSScriptRoot/../../init-script.ps1" | Out-Null
 }
 else {
   throw 'Cannot find init-script.ps1 file.'
@@ -11,10 +11,12 @@ if (!$IsWindows) {
   throw 'This script does not support operating systems other than Windows.'
 }
 
-if (Test-Path "${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk") {
+$path = "$Env:APPDATA/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk"
+
+if (Test-Path $path) {
   Write-Error 'Automatically locking on sign in is already enabled.'
 }
 else {
   'Enabling automatically locking on sign in'
-  New-Shortcut "${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk" 'C:/Windows/System32/rundll32.exe' -Parameters 'user32.dll, LockWorkStation'
+  New-Shortcut $path 'C:/Windows/System32/rundll32.exe' -Parameters 'user32.dll, LockWorkStation'
 }
