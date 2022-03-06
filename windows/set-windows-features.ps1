33edbc032744b0d38a08b67ef13203cbde5b3ee2
PlavorSeol <pseol2190@outlook.com>
2022-03-06 21:20:04 +0900

    Apply new coding conventions

--- windows/set-windows-features.ps1
+++ windows/set-windows-features.ps1
@@ -3,24 +3,22 @@
 param (
   # Parameter added just for making the -Verbose parameter work and does nothing
   [Parameter()]$x
-  )
+)
 
-if (Test-Path "${PSScriptRoot}/../init-script.ps1") {
-  ."${PSScriptRoot}/../init-script.ps1" | Out-Null
-  }
+if (Test-Path "$PSScriptRoot/../init-script.ps1") {
+  ."$PSScriptRoot/../init-script.ps1" | Out-Null
+}
 else {
   throw 'Cannot find init-script.ps1 file.'
-  }
+}
 
 if (!$IsWindows) {
   throw 'This script does not support operating systems other than Windows.'
-  }
-
-if (!(Test-AdminPermission)) {
+}
+elseif (!(Test-AdminPermission)) {
   throw 'This script requires administrator permission.'
-  }
+}
 
-# This is actually a hashtable and not PSCustomObject, but it works.
 $settings_object = @{
   disable = @(
     'MediaPlayback',
@@ -32,11 +30,11 @@ $settings_object = @{
     'SmbDirect',
     'WindowsMediaPlayer',
     'WorkFolders-Client'
-    )
+  )
   enable = @(
     'Microsoft-Windows-Subsystem-Linux'
-    )
-  }
+  )
+}
 
 $all_features_object = Get-WindowsOptionalFeature -Online
 
@@ -45,9 +43,9 @@ foreach ($action in 'disable', 'enable') {
 
   foreach ($feature in $features) {
     if ($feature -notin $all_features_object.FeatureName) {
-      Write-Error "${feature} is not a valid feature." -Category InvalidData
+      Write-Error "$feature is not a valid feature." -Category InvalidData
       continue
-      }
+    }
 
     $detailed_feature_object = Get-WindowsOptionalFeature -FeatureName $feature -Online
     $feature_display_name = $detailed_feature_object.DisplayName
@@ -55,22 +53,22 @@ foreach ($action in 'disable', 'enable') {
     switch ($action) {
       'disable' {
         if ($detailed_feature_object.State -eq 'Disabled') {
-          Write-Warning "${feature_display_name} (${feature}) feature is already disabled."
-          }
-        else {
-          "Disabling ${feature_display_name} (${feature}) feature"
-          Disable-WindowsOptionalFeature -FeatureName $feature -NoRestart -Online
-          }
+          Write-Warning "$feature_display_name ($feature) feature is already disabled."
         }
+        else {
+          "Disabling $feature_display_name ($feature) feature"
+          Disable-WindowsOptionalFeature -FeatureName $feature -NoRestart -Online
+        }
+      }
       'enable' {
         if ($detailed_feature_object.State -eq 'Enabled') {
-          Write-Warning "${feature_display_name} (${feature}) feature is already enabled."
-          }
+          Write-Warning "$feature_display_name ($feature) feature is already enabled."
+        }
         else {
-          "Enabling ${feature_display_name} (${feature}) feature"
+          "Enabling $feature_display_name ($feature) feature"
           Enable-WindowsOptionalFeature -FeatureName $feature -NoRestart -Online
-          }
         }
       }
     }
   }
+}
