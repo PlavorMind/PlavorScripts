77ba6a72bda89da12dc37d91109ac93ad6b9598b
PlavorSeol <pseol2190@outlook.com>
2022-03-06 21:37:07 +0900

    Apply new coding conventions

--- windows/lock-on-sign-in/disable.ps1
+++ windows/lock-on-sign-in/disable.ps1
@@ -1,7 +1,7 @@
 # Disables automatically locking on sign in.
 
-if (Test-Path "${PSScriptRoot}/../../init-script.ps1") {
-  ."${PSScriptRoot}/../../init-script.ps1" | Out-Null
+if (Test-Path "$PSScriptRoot/../../init-script.ps1") {
+  ."$PSScriptRoot/../../init-script.ps1" | Out-Null
 }
 else {
   throw 'Cannot find init-script.ps1 file.'
@@ -11,9 +11,11 @@ if (!$IsWindows) {
   throw 'This script does not support operating systems other than Windows.'
 }
 
-if (Test-Path "${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk") {
+$path = "$Env:APPDATA/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk"
+
+if (Test-Path $path) {
   'Disabling automatically locking on sign in'
-  Remove-Item "${Env:APPDATA}/Microsoft/Windows/Start Menu/Programs/Startup/Lock.lnk" -Force
+  Remove-Item $path -Force
 }
 else {
   Write-Error 'Automatically locking on sign in is not enabled.' -Category NotEnabled
