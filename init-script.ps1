4bbf212d6577d09b86e8e5b73a849c6846fe1929
PlavorSeol <pseol2190@outlook.com>
2022-03-06 21:14:56 +0900

    Apply new coding conventions

--- init-script.ps1
+++ init-script.ps1
@@ -1,15 +1,13 @@
 if ($PSVersionTable.PSVersion.Major -lt 7) {
   throw 'PlavorScripts does not support PowerShell 6 or older.'
 }
-
-if ($IsMacOS) {
+elseif ($IsMacOS) {
   throw 'PlavorScripts does not support macOS.'
 }
 elseif ($IsWindows -and ([Environment]::OSVersion.Version.Major -lt 10)) {
   throw 'PlavorScripts does not support Windows NT 6.3 (Windows 8.1) or older.'
 }
-
-if (![Environment]::Is64BitOperatingSystem) {
+elseif (![Environment]::Is64BitOperatingSystem) {
   throw 'PlavorScripts does not support 32-bit operating systems.'
 }
 
@@ -23,15 +21,15 @@ if ($IsLinux) {
 }
 elseif ($IsWindows) {
   $PlaScrDefaultBaseDirectory = 'C:/plavormind'
-  $PlaScrDefaultPHPPath = "${PlaScrDefaultBaseDirectory}/php/php.exe"
+  $PlaScrDefaultPHPPath = "$PlaScrDefaultBaseDirectory/php/php.exe"
   $PlaScrTempDirectory = $Env:TEMP
 }
 
 # Initialize environment variables
 $Env:POWERSHELL_TELEMETRY_OPTOUT = 1
 
-."${PlaScrDirectory}/src/common-functions.ps1"
-."${PlaScrDirectory}/src/legacy-functions.ps1"
+."$PlaScrDirectory/src/common-functions.ps1"
+."$PlaScrDirectory/src/legacy-functions.ps1"
 
 # Suppress warnings in VSCodium
 $PlaScrDefaultPHPPath | Out-Null
