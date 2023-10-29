0c6a480c0140f49924e54be78d77d70a893296ac
PlavorSeol <pseol2190@outlook.com>
2023-10-29 16:37:30 +0900

    Update common-functions.ps1

--- src/common-functions.ps1
+++ src/common-functions.ps1
@@ -5,9 +5,12 @@ Extracts an archive.
 function Expand-ArchiveEnhanced {
   param (
     # Path to save extracted items
-    [Parameter(Mandatory, Position = 1)][string]$DestinationPath,
-    # Archive path
-    [Parameter(Mandatory, Position = 0)][string]$Path
+    [Parameter(Mandatory, Position = 1)]
+    [string]$DestinationPath,
+
+    # Path to the archive
+    [Parameter(Mandatory, Position = 0)]
+    [string]$Path
   )
 
   if (!(Test-Path $Path)) {
@@ -15,56 +18,61 @@ function Expand-ArchiveEnhanced {
     return
   }
 
-  if ($Path -match '\.tar(\.[gx]z)?$') {
-    $extraParameters = @()
-
-    if ($VerbosePreference -eq 'Continue') {
-      $extraParameters += '-v'
-    }
-
-    switch ($Matches[1]) {
-      '.gz' {
-        $extraParameters += '-z'
-      }
-      '.xz' {
-        $extraParameters += '-J'
-      }
-    }
-
-    Write-Verbose "Extracting $Path archive"
-    tar -C $DestinationPath -f $Path -mx @extraParameters
+  try {
+    $normalizedPath = Convert-Path $Path -ErrorAction Stop
   }
-  elseif ((Split-Path $Path -Extension) -eq '.zip') {
+  catch {
+    Write-Error $_
+    return
+  }
+
+  if ($normalizedPath -match '\.tar(\.[gx]z)?$') {
+    Write-Verbose 'Creating a directory for extracting'
+
+    try {
+      New-Item $DestinationPath -ErrorAction Stop -Force -ItemType Directory
+    }
+    catch {
+      Write-Error $_
+      return
+    }
+
+    $extraParameters = $VerbosePreference -eq 'Continue' ? @('-v') : @()
+    $normalizedDestinationPath = Convert-Path $DestinationPath
+    $PSNativeCommandUseErrorActionPreference = $true
     Write-Verbose "Extracting $Path archive"
-    Expand-Archive $Path $DestinationPath -Force
+    tar -C $normalizedDestinationPath -f $normalizedPath -mx @extraParameters
+  }
+  elseif ((Split-Path $normalizedPath -Extension) -eq '.zip') {
+    Write-Verbose "Extracting $Path archive"
+    Expand-Archive $normalizedPath $DestinationPath -Force
   }
   else {
-    Write-Error 'Expand-ArchiveEnhanced does not support extracting this type of archive.' -Category NotImplemented
+    Write-Error 'Expand-ArchiveEnhanced does not support extracting this type of the archive.' -Category NotImplemented
   }
+
+  # Suppress a warning on VSCodium
+  $PSNativeCommandUseErrorActionPreference > $null
 }
 
 <#
 .description
-Downloads a file from specified URL.
+Downloads a file from a specified URL.
 #>
 function Get-FileFromURL {
   param (
-    # Path to save downloaded file
-    [Parameter(Mandatory, Position = 1)][string]$Path,
-    # URL to download a file
-    [Parameter(Mandatory, Position = 0)][string]$URL
+    # Path to save the downloaded file
+    [Parameter(Mandatory, Position = 1)]
+    [string]$Path,
+
+    # URL to download a file from
+    [Parameter(Mandatory, Position = 0)]
+    [string]$URL
   )
 
-  $ProgressPreferenceTemp = $ProgressPreference
   $ProgressPreference = 'SilentlyContinue'
   Write-Verbose "Downloading a file from $URL"
-
-  try {
-    Invoke-WebRequest $URL -HttpVersion 2.0 -MaximumRetryCount 2 -OutFile $Path -RetryIntervalSec 3
-  }
-  finally {
-    $ProgressPreference = $ProgressPreferenceTemp
-  }
+  Invoke-WebRequest $URL -HttpVersion 3.0 -MaximumRetryCount 2 -OutFile $Path -RetryIntervalSec 3
 }
 
 <#
@@ -74,11 +82,16 @@ Creates a shortcut. This function only supports Windows.
 function New-Shortcut {
   param (
     # Parameters to use when using the shortcut
-    [Parameter(Position = 2)][string]$Parameters,
-    # Shourtcut path
-    [Parameter(Mandatory, Position = 0)][string]$Path,
-    # Target of the shortcut
-    [Parameter(Mandatory, Position = 1)][string]$Target
+    [Parameter(Position = 2)]
+    [string]$Parameters,
+
+    # Path to create a shourtcut
+    [Parameter(Mandatory, Position = 0)]
+    [string]$Path,
+
+    # Path to the target of the shortcut
+    [Parameter(Mandatory, Position = 1)]
+    [string]$Target
   )
 
   if (!$IsWindows) {
@@ -92,7 +105,14 @@ function New-Shortcut {
 
   # -ComObject parameter must be specified otherwise New-Object causes an error.
   $shortcut = (New-Object -ComObject 'WScript.Shell').CreateShortcut($Path)
-  $shortcut.TargetPath = $Target
+
+  try {
+    $shortcut.TargetPath = Convert-Path $Target -ErrorAction Stop
+  }
+  catch {
+    Write-Error $_
+    return
+  }
 
   if ($Parameters -ne '') {
     $shortcut.Arguments = $Parameters
@@ -105,7 +125,7 @@ function New-Shortcut {
 
 <#
 .description
-Returns if the user has administrator permission on Windows, or root permission on Linux.
+Returns if a user has administrator permission on Windows, or root permission on Linux.
 #>
 function Test-AdminPermission {
   if ($IsLinux) {
@@ -120,13 +140,13 @@ function Test-AdminPermission {
 
 <#
 .description
-Returns if the PC is connected to internet.
+Returns if the PC is connected to the internet.
 #>
 function Test-InternetConnection {
-  Write-Verbose 'Checking internet connection by making a request to https://example.com/'
+  Write-Verbose 'Checking the internet connection by making a request to https://example.com/'
 
   try {
-    Invoke-WebRequest 'https://example.com/' -HttpVersion 2.0 -TimeoutSec 2 | Out-Null
+    Invoke-WebRequest 'https://example.com/' -HttpVersion 3.0 -TimeoutSec 2 > $null
   }
   catch {
     return $false
